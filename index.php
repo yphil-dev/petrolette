59ad0b65941cd30bc3cbbccdb5e3c307b694c2c8
yassinphilip <xaccrocheur@gmail.com>
2017-09-05 13:26:03 +0100

    Links in errors

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $feeds = 'feeds.xml';
 <!DOCTYPE html>
 <html>
 <head>
-<title>NWZ</title>
+<title>NeWS</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="robots" content="noindex,nofollow">
     <link rel="shortcut icon" type="image/x-icon" href="img/nws.png" />
--- nws-load-feed.php
+++ nws-load-feed.php
@@ -73,7 +73,7 @@ function reparse($u, $numItems, $imgMode, $photoblog) {
 
     $rssfeed = file_get_contents($u) or die("File Load / read error");
 
-    $feedRss = simplexml_load_string($rssfeed) or die("Feed Load / read error");
+    $feedRss = simplexml_load_string($rssfeed) or die($u." :error");
 
     $i = 0;
     $url = parse_url($u);
@@ -119,7 +119,7 @@ function reparse($u, $numItems, $imgMode, $photoblog) {
              <div class="feed" title ="'.$feedLink.'">
                  <div class="'.$title_class.'">
                      <span class="favicon">
-                         <a href="'.$u.'"><img src="'.$favicon.'" /></a>&nbsp;<a href="'.$feedLink.'" title="'.$title_link.' plop">'.$feedTitle.'</a>
+                         <a href="'.$u.'"><img src="'.$favicon.'" /></a>&nbsp;<a href="'.$feedLink.'" title="'.$title_link.'">'.$feedTitle.'</a>
                      </span>
 
                  </div>
