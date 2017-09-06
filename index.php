aaa99179d1e14413b836c74d2b46937af06be01e
yassinphilip <xaccrocheur@gmail.com>
2017-09-06 14:31:11 +0100

    Reactive: Mobile detection

--- index.php
+++ index.php
@@ -48,22 +48,23 @@ $feeds = 'feeds.xml';
         <script>
          
          $(document).ready(function() {
-
-             function detect-mobile() { 
-                 if (navigator.userAgent.match(/Android/i)
-                     || navigator.userAgent.match(/webOS/i)
-                     || navigator.userAgent.match(/iPhone/i)
-                     || navigator.userAgent.match(/iPad/i)
-                     || navigator.userAgent.match(/iPod/i)
-                     || navigator.userAgent.match(/BlackBerry/i)
-                     || navigator.userAgent.match(/Windows Phone/i))
-                     {
-                         return true;
-                     }
-                 else {
-                     return false;
-                 }
-             }
+             /* 
+              *              function detect-mobile() { 
+              *                  if (navigator.userAgent.match(/Android/i)
+              *                      || navigator.userAgent.match(/webOS/i)
+              *                      || navigator.userAgent.match(/iPhone/i)
+              *                      || navigator.userAgent.match(/iPad/i)
+              *                      || navigator.userAgent.match(/iPod/i)
+              *                      || navigator.userAgent.match(/BlackBerry/i)
+              *                      || navigator.userAgent.match(/Windows Phone/i))
+              *                      {
+              *                          return true;
+              *                      }
+              *                  else {
+              *                      return false;
+              *                  }
+              *              }
+              *              */
              /* 
               *              if (navigator.userAgent.match(/Android/i)
               *                  || navigator.userAgent.match(/webOS/i)
