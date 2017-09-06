6f2b8c9feafd3fe3fcc6fb656996a1784176f853
yassinphilip <xaccrocheur@gmail.com>
2017-09-06 13:19:59 +0100

    Div wrapping/flex: CSS tricks

--- index.php
+++ index.php
@@ -420,7 +420,7 @@ $feeds = 'feeds.xml';
 
                 foreach (array_keys($tabGroups) as $tabName) {
                     echo '
-    <div id="tab-'.$tabName.'" class="nws-feed">
+    <div id="tab-'.$tabName.'" class="nws-tab-feeds">
     <span class="gallery-tab" title="View all images in the ['.$tabName.'] tab">►</span>
 ';
                     foreach ($tabGroups[$tabName] as $tabUrl)
