4e7990a0784689c4597198f685a78c18d88db629
yassinphilip <xaccrocheur@gmail.com>
2017-09-06 15:09:59 +0100

    Feed error handling: MSG color

--- nws-load-feed.php
+++ nws-load-feed.php
@@ -85,7 +85,7 @@ function reparse($u, $numItems, $imgMode, $photoblog) {
     $feedRss = simplexml_load_string($rssfeed) or die('<div class="feed">
 <div class="feedTitle">
 <a title="'.$u.' is malformed (try wgetting it)\" href="'.$u.'">Error</a></div>
-<a title="'.$u.' is malformed (try wgetting it)\" href="'.$u.'"><span class="nws-error">The feed '.$u.' seems to be malformed/invalid</span> ; Try wgetting it to see what is wrong with it.</a></div>');
+<ul><li><a title="'.$u.' is malformed (try wgetting it)\" href="'.$u.'"><span class="nws-error">The feed '.$u.' seems to be malformed/invalid</span> ; Try wgetting it to see what is wrong with it.</a></li></ul></div>');
 
 // <div class="feed" title="https://davidlepee.com">
 //                  <div class="feedTitle">
