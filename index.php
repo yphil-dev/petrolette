6ed8887f22d88204f7376bf334cd0211b259026a
yassinphilip <xaccrocheur@gmail.com>
2017-09-06 14:16:36 +0100

    Reactive: Mobile detection

--- index.php
+++ index.php
@@ -46,30 +46,30 @@ $feeds = 'feeds.xml';
 
         
         <script>
-
-
-         function detectmob() { 
-             if( navigator.userAgent.match(/Android/i)
-              || navigator.userAgent.match(/webOS/i)
-              || navigator.userAgent.match(/iPhone/i)
-              || navigator.userAgent.match(/iPad/i)
-              || navigator.userAgent.match(/iPod/i)
-              || navigator.userAgent.match(/BlackBerry/i)
-              || navigator.userAgent.match(/Windows Phone/i)
-             ){
-                 return true;
-                 alert('Mobile!')
-             }
-             else {
-                 return false;
-                 alert('Desktop!')
-             }
-         }
          
          $(document).ready(function() {
 
 
-             alert(navigator.userAgent)
+
+             function detectmob() { 
+                 if( navigator.userAgent.match(/Android/i)
+                  || navigator.userAgent.match(/webOS/i)
+                  || navigator.userAgent.match(/iPhone/i)
+                  || navigator.userAgent.match(/iPad/i)
+                  || navigator.userAgent.match(/iPod/i)
+                  || navigator.userAgent.match(/BlackBerry/i)
+                  || navigator.userAgent.match(/Windows Phone/i)
+                 ){
+                     return true;
+                 }
+                 else {
+                     return false;
+                 }
+             }
+
+             if (detectmob) {
+                 alert(navigator.userAgent)
+             }
              
              $.ajaxSetup ({ cache: true })
 
