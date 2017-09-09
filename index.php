5bc02aa6663036c0f6b82814bd14eee91977ab2d
yassinphilip <xaccrocheur@gmail.com>
2017-09-10 00:37:13 +0100

    Layout button: Icons

--- index.php
+++ index.php
@@ -48,9 +48,9 @@ $feeds = 'feeds.xml';
 
            $('#nws-col-wrapping').click(function() {
                // alert('plop');
-               // $('.nws-tab-feeds').addClass('nws-tab-feeds-cols');
+               $('.nws-tab-feeds').removeClass('nws-tab-feeds-cols');
                // $(this).parent().removeClass('plop');
-               $(this).parent().removeClass('nws-tab-feeds-cols');
+               // $(this).parent().removeClass('nws-tab-feeds-cols');
 
            });
                
