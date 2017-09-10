e095741576c67d561730ca6c74eaf1c0db0b9676
yassinphilip <xaccrocheur@gmail.com>
2017-09-10 01:55:04 +0100

    Refactoring: JS code formatting

--- index.php
+++ index.php
@@ -45,138 +45,82 @@ $feeds = 'feeds.xml';
          
        $(document).ready(function() {
 
-
            $('.nws-button-col-wrapping').click(function() {
                $(this).parent().removeClass('nws-tab-feeds-cols').addClass('nws-tab-feeds-cols-block');
-               // $('.nws-tab-feeds').removeClass('nws-tab-feeds-cols');
                $(this).parent().children('.outerContainer').removeClass('outerContainer-cols').addClass('outerContainer-block');
            });
-               
-           /* 
-            *              if (navigator.userAgent.match(/Android/i)
-            *                  || navigator.userAgent.match(/webOS/i)
-            *                  || navigator.userAgent.match(/iPhone/i)
-            *                  || navigator.userAgent.match(/iPad/i)
-            *                  || navigator.userAgent.match(/iPod/i)
-            *                  || navigator.userAgent.match(/BlackBerry/i)
-            *                  || navigator.userAgent.match(/Windows Phone/i))
-            *                  {
-            *                      alert('Mobile!')
-            *                  }
-            *              else {
-            *                  alert('Not Mobile!')
-            *              }*/
-
              
+               $.ajaxSetup ({ cache: true });
 
-           var isMobile = false; //initiate as false
-           // device detection
-           if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
-              || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
+               $( "#tabs" ).tabs().find( ".ui-tabs-nav" ).sortable({ axis: "x" });
+
+               var totaltabs = $(".tabulators").find( "li" ).size();
+               var direction = null;
+               var ajax_loader = 'nws-load-feed.php';
+               var feed_max_age = 3600;
+               var ajax_spinner = '<img src="img/loading.gif" class="loading" alt="loading..." />';
              
-           /* if (isMobile) {
-            *     alert('Mobile!');
-            * }*/
-             
-           $.ajaxSetup ({ cache: true })
+               $('body').keyup(function(e) {
 
-                 $( "#tabs" ).tabs().find( ".ui-tabs-nav" ).sortable({ axis: "x" })
+                   direction = null;
 
-                 var totaltabs = $(".tabulators").find( "li" ).size()
-                 var direction = null
-                 var ajax_loader = 'nws-load-feed.php'
-                 var feed_max_age = 3600;
-           var ajax_spinner = '<img src="img/loading.gif" class="loading" alt="loading..." />'
-             
-                            $('body').keyup(function(e) {
+                   if (e.keyCode == 71) {
+                       if( $("#viewer").is(':visible') ) {
+                           $("#img-name a").trigger('click');
+                           
+                           window.location = $("#img-name a").attr('href');
+                       } else {
+                           alert("plop");
+                       };
+                   };
 
-                                // alert(e.keyCode + direction)
+                   if (e.keyCode == 37)
+                       if( $("#viewer").is(':visible') ) {
+                           $("#prev").trigger('click');
+                           direction = null;
+                       } else {
+                           direction = 'prev';
+                       };
 
-                                direction = null;
-
-                                if (e.keyCode == 71) {
-                                    if( $("#viewer").is(':visible') ) {
-                                        $("#img-name a").trigger('click')
-                                                    // alert($("#img-name a").attr('href'))
-                                                    // $("#img-name a").click()
-                                                    // $("#img-name a").css('border', '1px solid red')
-                                                    window.location = $("#img-name a").attr('href');
-                                    } else {
-                                        alert("plop")
-                                            }
-                                }
-
-                                // if (e.keyCode == 37) {
-                                //     direction = 'prev';
-                                // } else if (e.keyCode == 39) {
-                                //     direction = 'next'
-                                // } else {
-                                //     direction = null;
-                                // }
+                   if (e.keyCode == 39)
+                       if( $("#viewer").is(':visible') ) {
+                           $("#next").trigger('click');
+                           direction = null;
+                       } else {
+                           direction = 'next';
+                       };
 
 
-                                if (e.keyCode == 37)
-                                    if( $("#viewer").is(':visible') ) {
-                                        $("#prev").trigger('click')
-                                                    direction = null
-                                                    } else {
-                                        direction = 'prev'
-                                                  }
+                   if (e.keyCode == 27) {
+                       close_viewer();
+                   };
 
-                                if (e.keyCode == 39)
-                                    if( $("#viewer").is(':visible') ) {
-                                        $("#next").trigger('click')
-                                                    direction = null
-                                                    } else {
-                                        direction = 'next'
-                                                  }
-
-
-                                if (e.keyCode == 27) {
-                                    close_viewer()
-                                        }
-
-                                // if (e.keyCode == 32) {
-                                //     e.preventDefault()
-                                //     if ($("#play").is(':visible'))
-                                //         $("#play").trigger('click')
-                                //     else
-                                //         $("#pause").trigger('click')
-                                // }
-
-                                // $(document).keydown(function (e) {
-                                //     var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
-                                //     if ((key == 32) && (e.target.className != null) && (e.target.className.indexOf("ui-button") != -1))
-                                // });
+                   var active_tab = $("#tabs").tabs("option", "active");
+                                               
+                   if (direction != null)
+                       if (direction == 'next')
+                           if (active_tab < totaltabs -1)
+                               $("#tabs").tabs("option", "active", active_tab + 1);
+                           else
+                               $("#tabs").tabs("option", "active", 0);
+                       else
+                           if (active_tab != 0)
+                               $("#tabs").tabs("option", "active", active_tab - 1);
+                           else
+                               $("#tabs").tabs("option", "active", totaltabs - 1);
+               })
 
 
-                                var active_tab = $("#tabs").tabs("option", "active")
-
-                                               if (direction != null)
-                                                   if (direction == 'next')
-                                                       // $("#tabs").tabs("option", "active").find(".gallery-tab").show()
-                                                       if (active_tab < totaltabs -1)
-                                                           $("#tabs").tabs("option", "active", active_tab + 1)
-                                                           else
-                                                               $("#tabs").tabs("option", "active", 0)
-                                                               else
-                                                                   if (active_tab != 0)
-                                                                       $("#tabs").tabs("option", "active", active_tab - 1)
-                                                                       else
-                                                                           $("#tabs").tabs("option", "active", totaltabs - 1)
-                                                                                     })
+                        $("#tabs").bind("tabsactivate", function (event, ui) {
+                            document.title = ui.newTab.text() + " | NeWS"
+                                    });
 
 
-                            $("#tabs").bind("tabsactivate", function (event, ui) {
-                                document.title = ui.newTab.text() + " | NeWS"
-                                        });
-
-
-           function pulse() {
-               $('.moved').fadeIn(8000)
-                   $('.moved').fadeOut(200)
-                   }
-               setInterval(pulse, 150)
+               function pulse() {
+                   $('.moved').fadeIn(8000);
+                   $('.moved').fadeOut(200);
+               };
+               setInterval(pulse, 150);
 
                $('.nws-button-reload').click(function(){
                    var div_to_reload = $(this).parent()
@@ -323,9 +267,7 @@ $feeds = 'feeds.xml';
                $('.nws-button-gallery-tab').click(function(){
                    $("#overlay").show()
                                 var div_id = $(this).parent().children("div").first().attr("id")
-                                // var div_id = ''
                                 var tab_id = $(this).parent().attr("id")
-                                // alert("plop " + div_id)
                                 img_gallery(0, div_id, tab_id)
                                 })
 
@@ -482,7 +424,7 @@ $feeds = 'feeds.xml';
 
                 foreach (array_keys($tabGroups) as $tabName) {
                     echo '
-    <div id="tab-'.$tabName.'" class="nws-tab-feeds nws-tab-feeds-cols plop">
+    <div id="tab-'.$tabName.'" class="nws-tab-feeds nws-tab-feeds-cols">
     <span class="nws-button-col-wrapping" title="Change the column flowing style">&#128462;</span>
 
     <span class="nws-button-gallery-tab" title="View all images in the ['.$tabName.'] tab">►</span>
