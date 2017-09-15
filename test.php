209ed2e9959a120575f710c4d5d93d7c88267f5e
yassinphilip <xaccrocheur@gmail.com>
2017-09-15 17:28:05 +0100

    Drag & drop O-K, button down

--- test.php
+++ test.php
@@ -35,7 +35,72 @@
 
          $(document).ready(function() {
 
-             $.getScript("libs/test-tabs.js");
+             var tabTitle = $( "#tab_title" ),
+                 tabContent = $( "#tab_content" ),
+                 tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span></li>",
+                 tabCounter = 2;
+             
+             var tabs = $( "#tabs" ).tabs();
+             
+             // Modal dialog init: custom buttons and a "close" callback resetting the form inside
+             var dialog = $( "#dialog" ).dialog({
+                 autoOpen: false,
+                 modal: true,
+                 buttons: {
+                     Add: function() {
+                         addTab();
+                         $( this ).dialog( "close" );
+                     },
+                     Cancel: function() {
+                         $( this ).dialog( "close" );
+                     }
+                 },
+                 close: function() {
+                     form[ 0 ].reset();
+                 }
+             });
+             
+             // AddTab form: calls addTab function on submit and closes the dialog
+             var form = dialog.find( "form" ).on( "submit", function( event ) {
+                 addTab();
+                 dialog.dialog( "close" );
+                 event.preventDefault();
+             });
+             
+             // Actual addTab function: adds new tab using the input from the form above
+             function addTab() {
+                 var label = tabTitle.val() || "Tab " + tabCounter,
+                     id = "tabs-" + tabCounter,
+                     li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) ),
+                     tabContentHtml = tabContent.val() || "Tab " + tabCounter + " content.";
+                 
+                 tabs.find( ".ui-tabs-nav" ).append( li );
+                 tabs.append( "<div id='" + id + "'><p>" + tabContentHtml + "</p></div>" );
+                 tabs.tabs( "refresh" );
+                 tabCounter++;
+             }
+             
+             // AddTab button: just opens the dialog
+             $( "#add_tab" )
+                 .button()
+                 .on( "click", function() {
+                     dialog.dialog( "open" );
+                 });
+             
+             // Close icon: removing the tab on click
+             tabs.on( "click", "span.ui-icon-close", function() {
+                 var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+                 $( "#" + panelId ).remove();
+                 tabs.tabs( "refresh" );
+             });
+             
+             tabs.on( "keyup", function( event ) {
+                 if ( event.altKey && event.keyCode === $.ui.keyCode.BACKSPACE ) {
+                     var panelId = tabs.find( ".ui-tabs-active" ).remove().attr( "aria-controls" );
+                     $( "#" + panelId ).remove();
+                     tabs.tabs( "refresh" );
+                 }
+             });
 
              $('.elements').children().draggable({
                  appendTo: 'body',
@@ -86,8 +151,6 @@
             </form>
         </div>
 
-        <button id="add_tab">Add Tab</button>
-        
         <div id="tabs">
             <ul>
                 <li><a href="#tabs-1">plip</a></li>
@@ -107,6 +170,7 @@
                 </div>
             </div>
         </div>
+        <button id="add_tab">Add Tab</button>
         
     </body>
 </html>
