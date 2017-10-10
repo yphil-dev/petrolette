dd9021c0bd688ab70023cd951927dbcb845ba2d6
yassinphilip <xaccrocheur@gmail.com>
2017-10-10 12:04:59 +0100

    All: /feed route ACTUAL RSS return value OK

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var parser = require('rss-parser');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/feed', function(req, res, next) {
-    res.send('plop!');
-    console.log('im the feed!');
+    // res.send('plop!');
+    // console.log('im the feed!');
+
+    parser.parseURL('https://www.reddit.com/.rss', function(err, parsed) {
+        console.log(parsed.feed.title);
+        res.send(parsed.feed.title);
+
+        // parsed.feed.entries.forEach(function(entry) {
+        // console.log(entry.title + ':' + entry.link);
+        // })
+    });
+
+
 });
 
 
