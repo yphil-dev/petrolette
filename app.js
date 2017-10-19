8ee8e6766910d04ad42c0552fd4a63f3bbe74e46
yassinphilip <xaccrocheur@gmail.com>
2017-10-19 16:38:50 +0100

    Prefs: File UL prototype OK

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.use('/static', express.static(__dirname + '/public'));
+app.use('/tmp', express.static(__dirname + '/tmp'));
+
 app.use('/bower', express.static(__dirname + '/bower_components'));
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,17 @@ var express = require('express');
 var router = express.Router();
 var favicon = require('favicon');
 var parser = require('rss-parser');
+const fileUpload = require('express-fileupload');
 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
+
+router.use(fileUpload({
+    limits: { fileSize: 50 },
+    safeFileNames: true,
+    preserveExtension: true
+}));
 
 router.get('/feedicon', function(req, res, next) {
 
@@ -37,6 +44,22 @@ router.get('/feed', function(req, res, next) {
         if(typeof parsed != 'undefined') {
             res.send(parsed.feed);
         }
+    });
+});
+
+router.post('/upload', function(req, res) {
+    if (!req.files)
+        return res.status(400).send('No files were uploaded.');
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let sampleFile = req.files.sampleFile;
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv('/tmp/plop.json', function(err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.send('File uploaded!');
     });
 });
 
