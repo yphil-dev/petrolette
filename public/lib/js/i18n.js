8796f5220031192f50f979ad466cea09698f5747
yassinphilip <xaccrocheur@gmail.com>
2017-12-11 16:39:54 +0000

    UI: Yet another feedControls ergonomics paradigm

--- public/lib/js/i18n.js
+++ public/lib/js/i18n.js
@@ -67,8 +67,8 @@ MOB.i18n.translations = {
 
   "Source" : { fr: "Source" , ja: "RSSフィード", es: "Source" },
   "Sources" : { fr: "source" , ja: "RSSフィード" },
-  "Tab" : { fr: "groupe" , ja: "タブ", es: "Grupo" },
-  "Tabs" : { fr: "Groupes" , ja: "タブ", es: "Grupos" },
+  "Group" : { fr: "groupe" , ja: "タブ", es: "Grupo" },
+  "Groups" : { fr: "Groupes" , ja: "タブ", es: "Grupos" },
   "Theme" : { fr: "Thème" , ja: "テーマ", es: "Tema" },
   "Language" : { fr: "Langue" , ja: "言語", es: "Idioma" },
   "Image gallery" : { fr: "Galerie d'images" , ja: "イメージギャラリー", es: "Galería de imágenes" },
@@ -94,27 +94,27 @@ MOB.i18n.translations = {
   "Donate" : { fr: "Donation" , ja: "寄付する", es: "Donar" },
   "Open" : { fr: "Ouvrir" , ja: "負荷", es: "Carga" },
   "Save" : { fr: "Sauver" , ja: "セーブ", es: "Salvar" },
-  "Rename tab" : { fr: "Groupe: Renommer" , ja: "名前の変更タブ", es: "Cambiar el nombre de la grupo" },
-  "New tab" : { fr: "Nouveau Groupe" , ja: "新しいグループ", es: "Nuevo grupo" },
+  "Rename group" : { fr: "Groupe: Renommer" , ja: "名前の変更タブ", es: "Cambiar el nombre de la grupo" },
+  "New group" : { fr: "Nouveau Groupe" , ja: "新しいグループ", es: "Nuevo grupo" },
 
-  "Tab name" : { fr: "Nom du groupe" , ja: "グループ名", es: "Nombre de la grupo" },
+  "Group name" : { fr: "Nom du groupe" , ja: "グループ名", es: "Nombre de la grupo" },
   "Day theme" : { fr: "Thème jour" , ja: "日テーマ", es: "Tema del día" },
   "Night theme" : { fr: "Thème nuit" , ja: "夜のテーマ", es: "Tema nocturno" },
   "Day" : { fr: "Jour" , ja: "日", es: "día" },
   "Night" : { fr: "Nuit" , ja: "夜", es: "Noche" },
 
   // Help
-  "This is a tab. It contains sources." : { fr: "Ceci est un groupe ; un groupe contient des sources." , ja: "これはタブです。 それはソースを含んでいます。", es: "Esta es una grupo. Contiene fuentes." },
+  "This is a group. It contains sources." : { fr: "Ceci est un groupe ; un groupe contient des sources." , ja: "これはタブです。 それはソースを含んでいます。", es: "Esta es una grupo. Contiene fuentes." },
   "This is a source." : { fr: "Ceci est une source." , ja: "これはソースです", es: "Esta es una fuente." },
-  "Click this button to create a new tab." : { fr: "Cliquez sur ce bouton pour créer un nouvel groupe." , ja: "新しいタブを作成するには、このボタンをクリックします。", es: "Haga clic en este botón para crear una nueva grupo." },
+  "Click this button to create a new group." : { fr: "Cliquez sur ce bouton pour créer un nouvel groupe." , ja: "新しいタブを作成するには、このボタンをクリックします。", es: "Haga clic en este botón para crear una nueva grupo." },
   "Click here to add a source." : { fr: "Cliquer ici pour ajouter une source." , ja: "ソースを追加するには、このボタンをクリックします。", es: "Haga clic en este botón para agregar una fuente." },
   "What is Petrolette?" : { fr: "Qu'est-ce que Petrolette?" , ja: "Petroletteとは何ですか？", es: "¿Qué es Petrolette?" },
   "How do I use it?" : { fr: "Comment ça marche ?" , ja: "どうやって使うの？", es: "¿Como lo uso?" },
   "Petrolette is your news reader. Find new sources, sort them in categories, you are on the in-ter-net." : { fr: "Petrolette est un lecteur de sources. Trouver de nouvelles sources, les trier dans les catégories, c'est l'in-ter-net." , ja: "Petroletteはあなたのニュースリーダーです。 新しいソースを見つけ、カテゴリで並べ替え、あなたはインターネット上にいます。", es: "Petrolette es tu lector de noticias. Encuentra nuevas fuentes, clasifícalas en categorías, estás en la red interna." },
-  "Learn how to use <a id='aide-un' href='#'>the tabs and the sources, and Petrolette in general." : { fr: "Apprenez à utiliser <a id='aide-un' href='#'>les groupes et les sources</a>, et Petrolette en général." , ja: "タブとソース、および一般的なPetroletteの使い方を学んでください。", es: "Aprenda cómo usar <a id='aide-un' href='#'>las grupos y las fuentes</a>, y Petrolette en general." },
+  "Learn how to use <a id='aide-un' href='#'>the groups and the sources, and Petrolette in general." : { fr: "Apprenez à utiliser <a id='aide-un' href='#'>les groupes et les sources</a>, et Petrolette en général." , ja: "タブとソース、および一般的なPetroletteの使い方を学んでください。", es: "Aprenda cómo usar <a id='aide-un' href='#'>las grupos y las fuentes</a>, y Petrolette en general." },
   "Learn how to use" : { fr: "Apprenez à utiliser" , ja: "タブとソース、および一般的なPetroletteの使い方を学んでください。", es: "Aprenda cómo usar" },
   "and Petrolette in general." : { fr: "et Petrolette en général." , ja: "Petroletteの使い方を学んでください。", es: "y Petrolette en general." },
-  "the tabs and the sources" : { fr: "les groupes et les sources" , ja: "タブとソース", es: "las grupos y las fuentes" },
+  "the groups and the sources" : { fr: "les groupes et les sources" , ja: "タブとソース", es: "las grupos y las fuentes" },
 
 
 
@@ -129,11 +129,11 @@ MOB.i18n.translations = {
   "Hell, Yeah" : { fr: "Peste, Ouais" , ja: "地獄、うん。", es: "Diablos, Sí" },
   "Huh, Next question" : { fr: "Euh, Question suivante" , ja: "あ、次の質問", es: "Eh, Siguiente pregunta" },
 
-  "Open / import tabs and sources" : { fr: "Ouvrir / importer groupes et sources" , ja: "タブを開く/インポートする", es: "Abrir / importar grupos y canales RSS" },
-  "Save / Export tabs and sources" : { fr: "Enregistrer / exporter groupes et sources" , ja: "タブとフィードの保存/エクスポート", es: "Guardar archivo de tabs y sources" },
-  "Open tab on source drop" : { fr: "Ouvrir nouveau groupe" , ja: "ドロップ時にタブを開く", es: "Abrir grupo en drop" },
+  "Open / import groups and sources" : { fr: "Ouvrir / importer groupes et sources" , ja: "タブを開く/インポートする", es: "Abrir / importar grupos y canales RSS" },
+  "Save / Export groups and sources" : { fr: "Enregistrer / exporter groupes et sources" , ja: "タブとフィードの保存/エクスポート", es: "Guardar archivo de groups y sources" },
+  "Open group on source drop" : { fr: "Ouvrir nouveau groupe" , ja: "ドロップ時にタブを開く", es: "Abrir grupo en drop" },
 
-  "If this is set, when you drag & drop one or more source(s) in a tab, said tab opens" : { fr: "Ouvrir le groupe où une source est déposée" , ja: "魔法使いのタブを開いて餌を入れる", es: "Abrir grupo en donde se pone un canal" },
+  "If this is set, when you drag & drop one or more source(s) in a group, said group opens" : { fr: "Ouvrir le groupe où une source est déposée" , ja: "魔法使いのタブを開いて餌を入れる", es: "Abrir grupo en donde se pone un canal" },
 
   // Messages d'erreurs
   "View Petrolette according to the time of day." : { fr: "Voir Petrolette selon l'heure de la journée." , ja: "時刻に応じてPetroletteを表示する", es: "Ver Petrolette según la hora del día." },
@@ -144,7 +144,7 @@ MOB.i18n.translations = {
   "Help Petrolette according to your spiritual mood of the day." : { fr: "Aider (en vrai) Petrolette." , ja: "あなたの精神的な日の気分に応じてPetroletteを助けてください", es: "Ayuda a Petrolette de acuerdo con tu estado de ánimo espiritual del día." },
   "This file is bad" : { fr: "Ce fichier est chelou" , ja: "このファイルは悪いです", es: "Este archivo es malo" },
   "Loaded petrolette.json OK" : { fr: "Ouverture de petrolette.json OK" , ja: "このファイルは問題ありません", es: "Este archivo está bien" },
-  "Forgot what you are? Reset Petrolette tabs & sources" : { fr: "Oublié ce que vous êtes? Re-initialiser Petrolette" , ja: "あなたは何かを忘れましたか？ リセットPetrolette", es: "Olvidó lo que eres? Restablecer Petrolette" },
+  "Forgot what you are? Reset Petrolette groups & sources" : { fr: "Oublié ce que vous êtes? Re-initialiser Petrolette" , ja: "あなたは何かを忘れましたか？ リセットPetrolette", es: "Olvidó lo que eres? Restablecer Petrolette" },
 
   "You can't create more than %1 resources of each type." : { fr: "Vous ne pouvez pas créer plus de %1 ressources de chaque type.", ja: "各種％以上の財源を作ることはできません。" },
   "Error" : { fr: "Erreur", ja: "エラー", es: "Error" },
@@ -163,7 +163,7 @@ MOB.i18n.translations = {
   "How many new items should the source display at a time?" : { fr: "Combien de nouveaux articles cette source peut-elle contenir?", ja: "フィードは一度にいくつ表示する必要がありますか？", es: "¿Cuántos elementos nuevos debería mostrar el source a la vez?" },
   "Just answer the question" : { fr: "Répond juste à la question", ja: "ちょうど質問に答える", es: "Solo responde la pregunta" },
 
-  "Tab: Kill" : { fr: "Tab: Supprimer", ja: "タブ：削除", es: "Grupo: Eliminar" },
+  "Group: Kill" : { fr: "Group: Supprimer", ja: "タブ：削除", es: "Grupo: Eliminar" },
   "Source Location (URL)" : { fr: "Adresse de la source (URL)", ja: "場所（URL）", es: "Ubicación (URL)" },
   "Enter a website address/URL and click search, then OK, or simply enter the URL of the" : { fr: "Entrez l'adresse / URL d'un site Web, puis cliquez sur Rechercher, ou entrez directement l'adresse dun", ja: "ウェブサイトのアドレス/ URLを入力して[検索]をクリックし、次に[OK]をクリックするか、フィードの場所", es: "Ingrese la dirección / URL de un sitio web y haga clic en buscar, luego en Aceptar o ingrese directamente la ubicación del" },
 
@@ -175,10 +175,10 @@ MOB.i18n.translations = {
   "Fold / unfold" : { fr: "Plier / déplier", ja: "ウーブリール - ファーマー", es: "Abrir / cerrar" },
   "Select this source" : { fr: "Sélectionner cette source", ja: "RSSフィードを選択", es: "Selecciona este source" },
   "Delete this source" : { fr: "Supprimer cette source", ja: "このRSSフィードを削除", es: "Eliminar este source rss" },
-  "Delete the [%1] tab" : { fr: "Supprimer le groupe [%1]", ja: "サプリメント [%1]", es: "Eliminar este grupo [%1]" },
+  "Delete the [%1] group" : { fr: "Supprimer le groupe [%1]", ja: "サプリメント [%1]", es: "Eliminar este grupo [%1]" },
   "%1 | Click to rename, drag to re-order" : { fr: "%1 | Clic pour renommer, glisse pour ranger", ja: "%1 | クリックして名前を変更し、ドラッグして再注文します", es: "%1 | Haga clic para cambiar el nombre, arrastre para volver a ordenar" },
   "Really delete this source? (%1)" : { fr: "Réellement supprimer cette source (%1) ?", ja: "本当にこのフィードを削除しますか？  (%1)", es: "¿Realmente borraste este source? (%1)" },
-  "Really delete this tab? (%1, %2 sources)" : { fr: "Réellement supprimer cet groupe (%1, %2 sources) ?", ja: "本当にこのタブを削除しますか？ (%1, %2 フィード)", es: "¿Realmente borras esta grupo? (%1, %2 sources)" },
+  "Really delete this group? (%1, %2 sources)" : { fr: "Réellement supprimer ce groupe (%1, %2 sources) ?", ja: "本当にこのタブを削除しますか？ (%1, %2 フィード)", es: "¿Realmente borras esta grupo? (%1, %2 sources)" },
 
   "Options" : { fr: "Options", ja: "嗜好", es: "Preferencias" },
   "Refresh %1" : { fr: "Rafraîchir %1", ja: "リロード %1", es: "Recargar %1" },
--- public/lib/js/tab.js
+++ public/lib/js/tab.js
@@ -66,7 +66,7 @@ MOB.tab = {
   },
   makeNewTabButton:function($tabs) {
 
-    var $newTabButton = $('<li id="newTabButton" class="translate" data-title="New tab" title="New tab">').click(function () {
+    var $newTabButton = $('<li id="newTabButton" class="translate" data-title="New group" title="New group">').click(function () {
       MOB.tab.make($tabs);
       return false;
     });
