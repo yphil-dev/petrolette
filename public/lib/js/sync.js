d2ecf497ac492d681713332b23f85e5aa97ebfa5
yassinphilip <xaccrocheur@gmail.com>
2018-01-14 15:48:33 +0000

    Sync: privateClient.getFile maxAge values (false errors in Chromium) WIP

--- public/lib/js/sync.js
+++ public/lib/js/sync.js
@@ -10,7 +10,7 @@ MOB.sync = (function() {
       return {
         exports: {
           read: function () {
-            return privateClient.getFile('petrolette.conf', 90000000)
+            return privateClient.getFile('petrolette.conf', 90)
               .then(function (file) {
                 return file.data;
               });
