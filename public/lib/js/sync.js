75d8a1e700e34a4ed2e2dcf3575d0f527a5a18ce
yassinphilip <xaccrocheur@gmail.com>
2018-02-01 12:42:08 +0000

    CSS: Layout: Sync is broken *again* :|

--- public/lib/js/sync.js
+++ public/lib/js/sync.js
@@ -10,7 +10,7 @@ MOB.sync = (function() {
       return {
         exports: {
           read: function () {
-            return privateClient.getFile('petrolette.conf')
+            return privateClient.getFile('petrolette.conf', 9000000)
               .then(function (file) {
                 return file.data;
               });
