d5a2fd52490e6dfba527b3a2b2796529c0e9f80d
yassinphilip <xaccrocheur@gmail.com>
2018-02-20 13:34:49 +0000

    UI: Font size Firefox mobile fixing WIP

--- public/lib/js/utilities.js
+++ public/lib/js/utilities.js
@@ -8,12 +8,12 @@ MOB.utilities = {
   noSourcesButton:function() {
 
     var $loadingSpinner = $('<i>')
-          .attr('class', 'icon-spin3 spin');
+        .attr('class', 'icon-arrows-cw spin');
 
-      var $noSourcesText = $('<div>').text('Loading sources');
+    var $noSourcesText = $('<div>').text('Loading sources');
 
-      var $noSourcesButton = $('<div>')
-      // .append('<span data-content="Whoops, no sources!" data-title="Whoops, no sources!" class="translate"> Whoops, no sources! </span> <br/> <span data-content="Click here to add some." data-title="Click here to add some." class="translate"> Click here to add some.</span>')
+    var $noSourcesButton = $('<div>')
+    // .append('<span data-content="Whoops, no sources!" data-title="Whoops, no sources!" class="translate"> Whoops, no sources! </span> <br/> <span data-content="Click here to add some." data-title="Click here to add some." class="translate"> Click here to add some.</span>')
           .append($loadingSpinner)
           .append('<br>')
           .append($noSourcesText);
