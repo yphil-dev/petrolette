34f42ec341b0daf7cd8613b7f047cc9c1fdb78ca
yassinphilip <xaccrocheur@gmail.com>
2018-02-26 14:16:47 +0000

    UI: Menu: sourceCodeButton target fct

--- public/lib/js/menu.js
+++ public/lib/js/menu.js
@@ -5,6 +5,7 @@ $('<div id="menu">').appendTo($('body')).load('/static/templates/menu.html', fun
       $menuButton = $('#menuButton'),
       $helpButton = $('#helpButton'),
       $newSourceButton = $('#newSourceButton'),
+      $sourceCodeButton = $('button#sourceCode'),
       $importButton = $("button#fileImport"),
       $fileImportInput = $("input#fileImport"),
       $saveButton = $('#saveTabs'),
@@ -17,17 +18,22 @@ $('<div id="menu">').appendTo($('body')).load('/static/templates/menu.html', fun
 
   MOB.sync.attachWidget();
 
-  $menuButton.click(function () {
+  $menuButton.click(function() {
     $overlay.toggleClass('visible');
     $menu.toggleClass('expanded');
   });
 
-  $overlay.click(function () {
+  $sourceCodeButton.click(function(event) {
+    event.preventDefault();
+    window.open('https://framagit.org/yphil/petrolette');
+  });
+
+  $overlay.click(function() {
     $(this).removeClass('visible');
     $menu.removeClass('expanded');
   });
 
-  $newSourceButton.click(function () {
+  $newSourceButton.click(function() {
     var $openGroupPanel = $($('.ui-tabs-active').find('a').attr('href')).find('.tabSort');
     MOB.feed.make($openGroupPanel, 'New Feed', 'mixed', 8, true);
   });
