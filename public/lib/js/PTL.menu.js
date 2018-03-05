e2e3fe025677c8449489b27799374bb4cbb22e8b
yassinphilip <xaccrocheur@gmail.com>
2018-03-05 06:10:25 +0000

    Sys: Menu in module refactoring: Classes & IDs OK

--- public/lib/js/PTL.menu.js
+++ public/lib/js/PTL.menu.js
@@ -1,7 +1,14 @@
 PTL.menu = {
+  top:function() {
+
+    $('<div id="topMenu">').appendTo($('header')).load('/static/templates/menu.html nav#topnav', function() {
+      console.log('plop!');
+    });
+
+  },
   side:function() {
 
-    $('<div id="menu">').appendTo($('body')).load('/static/templates/menu.html', function() {
+    $('<div id="sideMenu">').appendTo($('body')).load('/static/templates/menu.html form#sideMenu', function() {
 
       var $menu = $(this),
           $overlay = $('#overlay'),
@@ -52,194 +59,194 @@ PTL.menu = {
       var $widget = $('#remotestorage-widget');
 
       var $readMore = $('<a>')
-          .attr('class', 'rs-help')
-          .attr('href', 'https:remotestorage.io/')
-          .text(PTL.tr('Read more.'));
+            .attr('class', 'rs-help')
+            .attr('href', 'https:remotestorage.io/')
+            .text(PTL.tr('Read more.'));
 
-      $widget.find('.rs-short-desc').text(PTL.tr('Pétrolette allows you to sync data with a storage of your choice ; '))
-        .append($readMore);
+        $widget.find('.rs-short-desc').text(PTL.tr('Pétrolette allows you to sync data with a storage of your choice ; '))
+          .append($readMore);
 
-      var $fuckingButton =  $widget.find('.rs-choose-rs');
+        var $fuckingButton =  $widget.find('.rs-choose-rs');
 
-      $fuckingButton.css('border-color', '#f00');
+        $fuckingButton.css('border-color', '#f00');
 
-      $fuckingButton.click(function (event) {
-        event.preventDefault();
-      });
+        $fuckingButton.click(function (event) {
+          event.preventDefault();
+        });
 
-      PTL.utilities.translate();
-
-      $langMenu.val(PTL.prefs.readConfig('lang')).prop('selected', true);
-
-      $langMenu.change(function() {
-        var selectedLang = $(this).val();
-        PTL.language = selectedLang;
-        PTL.prefs.writeConfig('lang', selectedLang);
         PTL.utilities.translate();
-      });
 
-      $helpButton.click(function (event) {
-        event.preventDefault();
-        $('#tabs').tabs('option', 'active', 0);
-        // PTL.utilities.help('ui');
-        PTL.dialog.help();
-      });
+        $langMenu.val(PTL.prefs.readConfig('lang')).prop('selected', true);
 
-      $donateButton.click(function (event) {
-        event.preventDefault();
-        window.open('https://liberapay.com/yPhil/donate', '_blank');
-      });
+        $langMenu.change(function() {
+          var selectedLang = $(this).val();
+          PTL.language = selectedLang;
+          PTL.prefs.writeConfig('lang', selectedLang);
+          PTL.utilities.translate();
+        });
 
-      $importButton.click(function () {
-        $("input#fileImport").click();
-        return false;
-      });
+        $helpButton.click(function (event) {
+          event.preventDefault();
+          $('#tabs').tabs('option', 'active', 0);
+          // PTL.utilities.help('ui');
+          PTL.dialog.help();
+        });
 
-      $saveButton.click(function () {
-        PTL.prefs.exportConfig(PTL.tab.list(), 'petrolette.conf');
-        return false;
-      });
+        $donateButton.click(function (event) {
+          event.preventDefault();
+          window.open('https://liberapay.com/yPhil/donate', '_blank');
+        });
 
-      $(".checkboxradio").checkboxradio({
-        icon: false
-      });
+        $importButton.click(function () {
+          $("input#fileImport").click();
+          return false;
+        });
 
-      $(this).find('input#' + PTL.prefs.readConfig('theme')).prop("checked", true)
-        .checkboxradio('refresh');
+        $saveButton.click(function () {
+          PTL.prefs.exportConfig(PTL.tab.list(), 'petrolette.conf');
+          return false;
+        });
 
-      $('.themeSwitcher').change(function() {
-        console.log('Theme: ' + '/static/css/themes/' + $(this).attr('value') + '.css');
+        $(".checkboxradio").checkboxradio({
+          icon: false
+        });
 
-        $("#mobStyle").attr({href : '/static/css/themes/' + $(this).attr('value') + '.css'});
+        $(this).find('input#' + PTL.prefs.readConfig('theme')).prop("checked", true)
+          .checkboxradio('refresh');
 
-        PTL.prefs.writeConfig('theme', $(this).attr('value'));
+        $('.themeSwitcher').change(function() {
+          console.log('Theme: ' + '/static/css/themes/' + $(this).attr('value') + '.css');
 
-      });
+          $("#mobStyle").attr({href : '/static/css/themes/' + $(this).attr('value') + '.css'});
 
-      var gallerySlideshowSpeed = PTL.prefs.readConfig('gallerySlideshowSpeed');
-      var gallerySlideTransition = PTL.prefs.readConfig('gallerySlideTransition');
+          PTL.prefs.writeConfig('theme', $(this).attr('value'));
 
-      // $.fancybox.defaults.thumbs.autoStart = true;
-      $.fancybox.defaults.transitionEffect = gallerySlideTransition;
-      $.fancybox.defaults.slideShow.speed = gallerySlideshowSpeed;
+        });
 
-      $menu.find('select#gallerySlideTransition').change(function() {
-        console.log('New FX: %s', $(this).val());
-        $.fancybox.defaults.transitionEffect = $(this).val();
-        PTL.prefs.writeConfig('gallerySlideTransition', $(this).val());
-      });
+        var gallerySlideshowSpeed = PTL.prefs.readConfig('gallerySlideshowSpeed');
+        var gallerySlideTransition = PTL.prefs.readConfig('gallerySlideTransition');
 
-      $menu.find('select#gallerySlideTransition').val(gallerySlideTransition);
+        // $.fancybox.defaults.thumbs.autoStart = true;
+        $.fancybox.defaults.transitionEffect = gallerySlideTransition;
+        $.fancybox.defaults.slideShow.speed = gallerySlideshowSpeed;
 
-      if (PTL.prefs.readConfig('tabDropActivate') === 'true')
-        $('input#tabDropActivate').prop('checked', true).checkboxradio('refresh');
-      else
-        $('input#tabDropActivate').prop('checked', false).checkboxradio('refresh');
+        $menu.find('select#gallerySlideTransition').change(function() {
+          console.log('New FX: %s', $(this).val());
+          $.fancybox.defaults.transitionEffect = $(this).val();
+          PTL.prefs.writeConfig('gallerySlideTransition', $(this).val());
+        });
 
-      $('input#tabDropActivate').change(function() {
-        PTL.prefs.writeConfig('tabDropActivate', $(this).prop('checked'));
-      });
+        $menu.find('select#gallerySlideTransition').val(gallerySlideTransition);
 
-      $spinner.spinner({
-        min: 0.5,
-        max: 10000,
-        step: 0.5,
-        classes: {
-          "ui-spinner": "shrink ui-corner-all",
-          "ui-spinner-down": "ui-corner-br",
-          "ui-spinner-up": "ui-corner-tr"
-        }
-      });
+        if (PTL.prefs.readConfig('tabDropActivate') === 'true')
+          $('input#tabDropActivate').prop('checked', true).checkboxradio('refresh');
+        else
+          $('input#tabDropActivate').prop('checked', false).checkboxradio('refresh');
 
-      $spinner.spinner('value', PTL.utilities.milliToSecs(gallerySlideshowSpeed));
+        $('input#tabDropActivate').change(function() {
+          PTL.prefs.writeConfig('tabDropActivate', $(this).prop('checked'));
+        });
 
-      $spinner.on( 'spinstop', function() {
-        $slider.slider( 'option', 'value', $(this).val() * 1000);
-        $('.ui-slider-handle').text(PTL.utilities.milliToSecs($(this).val() * 1000) + 's');
-      });
+        $spinner.spinner({
+          min: 0.5,
+          max: 10000,
+          step: 0.5,
+          classes: {
+            "ui-spinner": "shrink ui-corner-all",
+            "ui-spinner-down": "ui-corner-br",
+            "ui-spinner-up": "ui-corner-tr"
+          }
+        });
 
-      $slider.slider({
-        classes: {
-          "ui-slider": "grow ui-corner-all",
-          "ui-slider-handle": "ui-corner-all",
-          "ui-slider-range": "ui-corner-all ui-widget-header"
-        },
-        value: gallerySlideshowSpeed,
-        min: 500,
-        max: 10000,
-        step: 500,
-        create: function() {
-          $(this).find('.ui-slider-handle').text(PTL.utilities.milliToSecs(gallerySlideshowSpeed) + 's');
-        },
-        slide: function(event, ui) {
-          // $('#gallerySlideshowSpeedValue').text(ui.value + 'ms');
-          $spinner.val(PTL.utilities.milliToSecs(ui.value));
-          $(this).find('.ui-slider-handle').text(PTL.utilities.milliToSecs(ui.value) + 's');
+        $spinner.spinner('value', PTL.utilities.milliToSecs(gallerySlideshowSpeed));
 
-        },
-        change: function(event, ui) {
-          PTL.prefs.writeConfig('gallerySlideshowSpeed', ui.value);
-          $('#tabs').find("[data-fancybox]").fancybox({
-            slideShow: {
-              speed: ui.value
-            }
-          });
-        }
-      });
+        $spinner.on( 'spinstop', function() {
+          $slider.slider( 'option', 'value', $(this).val() * 1000);
+          $('.ui-slider-handle').text(PTL.utilities.milliToSecs($(this).val() * 1000) + 's');
+        });
 
-      // File reader
+        $slider.slider({
+          classes: {
+            "ui-slider": "grow ui-corner-all",
+            "ui-slider-handle": "ui-corner-all",
+            "ui-slider-range": "ui-corner-all ui-widget-header"
+          },
+          value: gallerySlideshowSpeed,
+          min: 500,
+          max: 10000,
+          step: 500,
+          create: function() {
+            $(this).find('.ui-slider-handle').text(PTL.utilities.milliToSecs(gallerySlideshowSpeed) + 's');
+          },
+          slide: function(event, ui) {
+            // $('#gallerySlideshowSpeedValue').text(ui.value + 'ms');
+            $spinner.val(PTL.utilities.milliToSecs(ui.value));
+            $(this).find('.ui-slider-handle').text(PTL.utilities.milliToSecs(ui.value) + 's');
 
-      $fileImportInput.change(function(evt){
-        var f = evt.target.files[0],
-            reader = new FileReader();
+          },
+          change: function(event, ui) {
+            PTL.prefs.writeConfig('gallerySlideshowSpeed', ui.value);
+            $('#tabs').find("[data-fancybox]").fancybox({
+              slideShow: {
+                speed: ui.value
+              }
+            });
+          }
+        });
 
-        reader.onload = (function() {
-          return function(e) {
+        // File reader
 
-            function isOk(o) {
-              var isValid = false;
-              if (Object.prototype.toString.call(o) === '[object Array]') {
-                isValid = o.some(obj => Array.isArray(obj.feeds) && obj.feeds.some(feed => Object.prototype.hasOwnProperty.call(feed, 'url')));
+        $fileImportInput.change(function(evt){
+          var f = evt.target.files[0],
+              reader = new FileReader();
+
+          reader.onload = (function() {
+            return function(e) {
+
+              function isOk(o) {
+                var isValid = false;
+                if (Object.prototype.toString.call(o) === '[object Array]') {
+                  isValid = o.some(obj => Array.isArray(obj.feeds) && obj.feeds.some(feed => Object.prototype.hasOwnProperty.call(feed, 'url')));
+                } else {
+                  isValid = false;
+                }
+                return isValid;
+              }
+
+              var y = e.target.result;
+
+              function isJsonString(str) {
+                try {
+                  JSON.parse(str);
+                } catch (e) {
+                  return false;
+                }
+                return true;
+              }
+
+              var p = false;
+
+              if (isJsonString(y)) {
+                p = JSON.parse(y);
               } else {
-                isValid = false;
+                console.error('Pétrolette | ' + PTL.tr('This file is bad'));
               }
-              return isValid;
-            }
 
-            var y = e.target.result;
-
-            function isJsonString(str) {
-              try {
-                JSON.parse(str);
-              } catch (e) {
-                return false;
+              if (p && isOk(p) === true){
+                console.info('Pétrolette | ' + PTL.tr('Loading of [%s] OK', f.name));
+                PTL.tab.populate(p, true);
+              } else {
+                console.error('Pétrolette | ' + PTL.tr('This file is bad'));
               }
-              return true;
-            }
 
-            var p = false;
+            };
+          })(f);
 
-            if (isJsonString(y)) {
-              p = JSON.parse(y);
-            } else {
-              console.error('Pétrolette | ' + PTL.tr('This file is bad'));
-            }
+          reader.readAsText(f);
 
-            if (p && isOk(p) === true){
-              console.info('Pétrolette | ' + PTL.tr('Loading of [%s] OK', f.name));
-              PTL.tab.populate(p, true);
-            } else {
-              console.error('Pétrolette | ' + PTL.tr('This file is bad'));
-            }
-
-          };
-        })(f);
-
-        reader.readAsText(f);
+        });
 
       });
 
-    });
-
-  }
+    }
 };
