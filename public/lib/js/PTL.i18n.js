d296158e50d14212bf05451c4ad2e0a125c5a913
yassinphilip <xaccrocheur@gmail.com>
2018-03-06 16:22:38 +0000

    Version %s - 06/03/2018 16:22:38

--- public/lib/js/PTL.i18n.js
+++ public/lib/js/PTL.i18n.js
@@ -220,8 +220,6 @@ PTL.i18n.translations = {
 
   "Problem reading source [%1] Error type [%2]" : { fr: "Problème lecture source [%1] Type d'erreur [%2]", ja: "問題の読書の問題[%1]エラータイプ[%2]", es: "Problema al leer la fuente [%1] Tipo de error [%2]" },
 
-
-
   "ERROR TYPE" : { fr: "TYPE D'ERREUR", ja: "エラータイプ", es: "TIPO DE ERROR" },
 
   "Error" : { fr: "Erreur", ja: "エラー", es: "Error" },
