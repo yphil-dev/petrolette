29f51b9e7d957be9c566d8f79d4f6dffa05b63a4
yassinphilip <xaccrocheur@gmail.com>
2018-03-08 16:13:01 +0000

    Sys: Licencing info: All Pétrolette PASSED LibreJS parsing

--- public/js/PTL.utilities.js
+++ public/js/PTL.utilities.js
@@ -1,3 +1,5 @@
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
+
 PTL.utilities = {
   isValidSourcesFile:function(sources) {
 
@@ -305,3 +307,5 @@ PTL.utilities = {
       return parseFloat(secs + '.' + ms.toFixed(1));
     }
 };
+
+// @license-end
