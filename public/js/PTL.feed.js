692af56167f47237754460f499a1ea04f2fa3893
yassinphilip <xaccrocheur@gmail.com>
2021-02-04 23:18:19 +0100

    Two seasons, in 4 languages

--- public/js/PTL.feed.js
+++ public/js/PTL.feed.js
@@ -227,8 +227,6 @@ PTL.feed = {
 
           if (data.error || data.feedItems.length == 0) {
 
-            console.log('data: ', data.error.code);
-
             var message = data.error.code ? data.error.code : PTL.tr('Empty feed');
 
             PTL.util.console(PTL.tr('Problem reading feed [%1] Error type [%2]', feedUrl, message), 'warning');
