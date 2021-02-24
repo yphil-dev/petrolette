fec7e330e7bbe9dfd4a934c830ee602754671a4b
yassinphilip <xaccrocheur@gmail.com>
2021-02-24 12:18:03 +0100

    Moved feeder to its own lib

---
+++ routes/feeder.js
@@ -0,0 +1,90 @@
+const request = require('request'),
+      FeedParser = require('feedparser');
+
+exports.getFeed = getFeed; 
+
+function maybeDecompress (res, encoding) {
+  var decompress;
+  if (encoding.match(/\bdeflate\b/)) {
+    decompress = zlib.createInflate();
+  } else if (encoding.match(/\bgzip\b/)) {
+    decompress = zlib.createGunzip();
+  }
+  return decompress ? res.pipe(decompress) : res;
+}
+
+function maybeTranslate (res, charset) {
+  var iconv;
+  // Use iconv if its not utf8 already.
+  if (!iconv && charset && !/utf-*8/i.test(charset)) {
+    try {
+      iconv = new Iconv(charset, 'utf-8');
+      console.log('Converting from charset %s to utf-8', charset);
+      iconv.on('error', done);
+      // If we're using iconv, stream will be the output of iconv
+      // otherwise it will remain the output of request
+      res = res.pipe(iconv);
+    } catch(err) {
+      res.emit('error', err);
+    }
+  }
+  return res;
+}
+
+function getParams(str) {
+  var params = str.split(';').reduce(function (params, param) {
+    var parts = param.split('=').map(function (part) { return part.trim(); });
+    if (parts.length === 2) {
+      params[parts[0]] = parts[1];
+    }
+    return params;
+  }, {});
+  return params;
+}
+
+function getFeed (urlfeed, callback) {
+
+  var req = request(urlfeed, {timeout: 10000, pool: false});
+  req.setMaxListeners(50);
+  req.setHeader('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
+  req.setHeader('accept', 'text/html,application/xhtml+xml');
+
+  var feedparser = new FeedParser ();
+  var feedItems = [];
+
+  req
+    .on ('error', function (err) {
+      callback(err);
+    })
+    .on ('response', function (res) {
+      if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+      var encoding = res.headers['content-encoding'] || 'identity',
+          charset = getParams(res.headers['content-type'] || '').charset;
+      res = maybeDecompress(res, encoding);
+      res = maybeTranslate(res, charset);
+      res.pipe (feedparser);
+    });
+
+  feedparser
+    .on ('readable', function () {
+      try {
+        var item = this.read ();
+        if (item !== null) feedItems.push (item);
+      }
+      catch (err) {
+        console.log('ERR (%s)', err.message);
+      }
+    })
+    .on ('error', function (err) {
+      var meta = this.meta;
+      console.error('HUUM (%s) %s %s', err.message, meta.title, urlfeed);
+      callback (err.message);
+    })
+    .on ('end', function () {
+      var meta = this.meta;
+
+      callback ('Feed OK', feedItems, meta.title, meta.link);
+
+      return;
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express'),
       router = express.Router(),
       favrat = require('favrat'),
-      FeedParser = require('feedparser'),
+      feeder = require('./feeder'),
       request = require('request'),
       feedrat = require('feedrat'),
       // feedrat = require(__dirname + '/../../feedrat/'),
@@ -22,92 +22,6 @@ process.on('uncaughtException', function(err) {
   console.error('### Pétrolette uncaughtException: %s', err);
 });
 
-function maybeDecompress (res, encoding) {
-  var decompress;
-  if (encoding.match(/\bdeflate\b/)) {
-    decompress = zlib.createInflate();
-  } else if (encoding.match(/\bgzip\b/)) {
-    decompress = zlib.createGunzip();
-  }
-  return decompress ? res.pipe(decompress) : res;
-}
-
-function maybeTranslate (res, charset) {
-  var iconv;
-  // Use iconv if its not utf8 already.
-  if (!iconv && charset && !/utf-*8/i.test(charset)) {
-    try {
-      iconv = new Iconv(charset, 'utf-8');
-      console.log('Converting from charset %s to utf-8', charset);
-      iconv.on('error', done);
-      // If we're using iconv, stream will be the output of iconv
-      // otherwise it will remain the output of request
-      res = res.pipe(iconv);
-    } catch(err) {
-      res.emit('error', err);
-    }
-  }
-  return res;
-}
-
-function getParams(str) {
-  var params = str.split(';').reduce(function (params, param) {
-    var parts = param.split('=').map(function (part) { return part.trim(); });
-    if (parts.length === 2) {
-      params[parts[0]] = parts[1];
-    }
-    return params;
-  }, {});
-  return params;
-}
-
-function getFeed (urlfeed, callback) {
-
-  var req = request(urlfeed, {timeout: 10000, pool: false});
-  req.setMaxListeners(50);
-  req.setHeader('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
-  req.setHeader('accept', 'text/html,application/xhtml+xml');
-
-  var feedparser = new FeedParser ();
-  var feedItems = [];
-
-  req
-    .on ('error', function (err) {
-      callback(err);
-    })
-    .on ('response', function (res) {
-      if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
-      var encoding = res.headers['content-encoding'] || 'identity',
-          charset = getParams(res.headers['content-type'] || '').charset;
-      res = maybeDecompress(res, encoding);
-      res = maybeTranslate(res, charset);
-      res.pipe (feedparser);
-    });
-
-  feedparser
-    .on ('readable', function () {
-      try {
-        var item = this.read ();
-        if (item !== null) feedItems.push (item);
-      }
-      catch (err) {
-        console.log('ERR (%s)', err.message);
-      }
-    })
-    .on ('error', function (err) {
-      var meta = this.meta;
-      console.error('HUUM (%s) %s %s', err.message, meta.title, urlfeed);
-      callback (err.message);
-    })
-    .on ('end', function () {
-      var meta = this.meta;
-
-      callback ('Feed OK', feedItems, meta.title, meta.link);
-
-      return;
-  });
-}
-
 router.use(sanitize);
 
 router.get('/feed', function(req, res) {
@@ -122,7 +36,7 @@ router.get('/feed', function(req, res) {
     })
     .on('response', function(response) {
 
-      getFeed(req.query.feedurl, function (err, feedItems, feedTitle, feedLink) {
+      feeder.getFeed(req.query.feedurl, function (err, feedItems, feedTitle, feedLink) {
 
         if (feedItems && !res.headersSent) {
           res.send({
