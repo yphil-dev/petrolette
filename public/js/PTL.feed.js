5bdd9341ab9f0da75c7e5dc26b56a54e12c89d56
yassinphilip <xaccrocheur@gmail.com>
2021-03-03 22:45:32 +0100

    Video player, new lazy loading.gif, looser robots.txt, new audio icon, new default feeds

--- public/js/PTL.feed.js
+++ public/js/PTL.feed.js
@@ -331,10 +331,12 @@ PTL.feed = {
 
           var $imageLink = $('<a>').attr('target', '_blank'),
               $itemLink = $('<a>').attr('target', '_blank'),
-              $soundLink = $('<a>').attr('target', '_blank'),
+              $audioLink = $('<a>').attr('target', '_blank'),
+              $videoLink = $('<a>').attr('target', '_blank'),
               $commentsLink = $('<a>').attr('target', '_blank'),
               $commentsIcon = $('<i>'),
-              $soundIcon = $('<i>'),
+              $audioIcon = $('<i>'),
+              $videoIcon = $('<i>'),
               $image,
               $summary = $('<null>').append(PTL.util.sanitizeInput(summary)).text(),
               $itemDiv = $('<div>').attr('class', 'itemDiv'),
@@ -396,6 +398,25 @@ PTL.feed = {
               imageUrl = item.enclosures[0].url;
             }
 
+            if (item.enclosures[0].url && item.enclosures[0].url.match(/\.(mp4|webm)$/)) {
+              console.log('Video: %s (%s)', item.enclosures[0].url, item.enclosures[0].type);
+
+              var videoPlayer      = document.createElement('video');
+              // videoPlayer.id       = 'video-player';
+              videoPlayer.controls = 'controls';
+              videoPlayer.src      = item.enclosures[0].url;
+              videoPlayer.type     = item.enclosures[0].type;
+
+              $itemDiv.append(videoPlayer);
+
+              $videoLink
+                .attr('href', item.enclosures[0].url)
+                .appendTo($itemDiv);
+              $videoIcon
+                .attr('class', 'item-icon icon-video')
+                .appendTo($videoLink);
+            }
+
             if (item.enclosures[0].url && item.enclosures[0].url.match(/\.(ogg|mp3)$/)) {
 
               var audioPlayer      = document.createElement('audio');
@@ -403,15 +424,16 @@ PTL.feed = {
               audioPlayer.controls = 'controls';
               audioPlayer.src      = item.enclosures[0].url;
               audioPlayer.type     = item.enclosures[0].type;
+              audioPlayer.preload  = 'metadata';
 
               $itemDiv.append(audioPlayer);
 
-              $soundLink
+              $audioLink
                 .attr('href', item.enclosures[0].url)
                 .appendTo($itemDiv);
-              $soundIcon
+              $audioIcon
                 .attr('class', 'item-icon icon-audio')
-                .appendTo($soundLink);
+                .appendTo($audioLink);
             }
           }
 
@@ -458,9 +480,9 @@ PTL.feed = {
               .attr('data-caption', '<a href="' + item.link + '" class="ui-button ui-corner-all" title="' + $summary.trim() + '">' + item.title + '</a>');
 
             $image = $('<img>')
-              .attr('src', '/static/images/loading.jpg')
+              .attr('src', '/static/images/loading.gif')
               .attr('data-srcset', imageUrl)
-              .attr('srcset', 'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==')
+              .attr('srcset', '/static/images/loading.gif')
               .attr('title', $summary.trim())
               .attr('alt', item['mastodon:scope'] ? $summary.trim() : item.title)
               .attr('class', 'ptl-img responsively-lazy')
--- public/js/PTL.prefs.js
+++ public/js/PTL.prefs.js
@@ -7,15 +7,13 @@ PTL.prefs = (function() {
      "columns": [
        [
          {"url":"https://www.sciencemag.org/rss/weekly_news_email.xml","type":"photo","limit": 200,"status":"off"},
-         {"url":"http://rss.sciam.com/ScientificAmerican-Global","type":"mixed","limit": 450,"status":"on"},
-         {"url":"https://www.popsci.com/arcio/rss/","type":"mixed","limit": 450,"status":"off"},
+         {"url":"https://www.popsci.com/arcio/rss/","type":"mixed","limit": 300,"status":"on"},
 
-         {"url":"https://www.wired.com/category/science/feed","type":"mixed","limit": 450,"status":"on"}
+         {"url":"https://www.wired.com/category/science/feed","type":"mixed","limit": 300,"status":"on"}
        ],
        [
-         {"url":"https://www.reddit.com/r/worldnews.rss","type":"mixed","limit": 450,"status":"off"},
          {"url":"https://rss.art19.com/apology-line","type":"text","limit": 40,"status":"off"},
-         {"url":"http://feeds.feedburner.com/sciencealert-latestnews","type":"mixed","limit": 450,"status":"on"},
+         {"url":"http://feeds.feedburner.com/sciencealert-latestnews","type":"mixed","limit": 300,"status":"on"},
          {"url":"https://www.npr.org/rss/rss.php?id=1001","type":"mixed","limit": 400,"status":"on"}
        ],
        [
@@ -24,22 +22,23 @@ PTL.prefs = (function() {
          {"url":"http://explainxkcd.com/rss.xml","type":"photo","limit": 60,"status":"on"}
        ],
        [
+         {"url":"http://faif.us/feeds/cast-ogg/","type":"text","limit": 250,"status":"on"},
          {"url":"https://www.yellowtrace.com.au/category/architecture/feed/","type":"photo","limit": 200,"status":"off"},
-         {"url":"https://www.ignant.com/category/architecture/feed/","type":"photo","limit": 450,"status":"on"}
+         {"url":"https://www.ignant.com/category/architecture/feed/","type":"photo","limit": 300,"status":"on"}
        ]
      ]
     },
     {"name":"tek",
      "columns": [
        [
-         {"url":"http://feeds.bbci.co.uk/news/technology/rss.xml","type":"mixed","limit": 450,"status":"on"},
-         {"url":"http://feeds.feedburner.com/hackaday/LgoM/","type":"mixed","limit": 450,"status":"on"},
+         {"url":"http://feeds.bbci.co.uk/news/technology/rss.xml","type":"mixed","limit": 300,"status":"on"},
+         {"url":"http://feeds.feedburner.com/hackaday/LgoM/","type":"mixed","limit": 300,"status":"on"},
 
-         {"url":"https://hackernoon.com/feed","type":"mixed","limit": 450,"status":"off"}
+         {"url":"https://hackernoon.com/feed","type":"mixed","limit": 300,"status":"off"}
        ],
        [
          {"url":"https://hacks.mozilla.org/feed/","type":"mixed","limit": 400,"status":"on"},
-         {"url":"http://feeds.arstechnica.com/arstechnica/index","type":"mixed","limit": 450,"status":"on"}
+         {"url":"http://feeds.arstechnica.com/arstechnica/index","type":"mixed","limit": 300,"status":"on"}
        ],
        [
          {"url":"https://www.technologyreview.com/topnews.rss","type":"mixed","limit": 400,"status":"on"},
@@ -52,12 +51,12 @@ PTL.prefs = (function() {
     {"name":"img",
      "columns": [
        [
-         {"url":"http://cabinporn.com/rss/","type":"photo","limit": 250,"status":"off"},
-         {"url":"https://iso.500px.com/feed/","type":"photo","limit": 250,"status":"on"}
+         {"url":"http://cabinporn.com/rss/","type":"photo","limit": 450,"status":"off"},
+         {"url":"https://iso.500px.com/feed/","type":"photo","limit": 350,"status":"on"}
        ],
        [
-         {"url":"http://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss","type":"photo","limit": 250,"status":"off"},
-         {"url":"https://mastodon.social/@256.rss","type":"photo","limit": 250,"status":"on"}
+         {"url":"http://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss","type":"photo","limit": 450,"status":"off"},
+         {"url":"https://mastodon.social/@256.rss","type":"photo","limit": 220,"status":"on"}
        ]
 
      ]
@@ -65,34 +64,32 @@ PTL.prefs = (function() {
     {"name":"social",
      "columns": [
        [
-         {"url":"https://mastodon.social/@Gargron.rss","type":"mixed","limit": 450,"status":"off"},
-         {"url":"https://bitcoinhackers.org/@mastodonusercount.rss","type":"photo","limit": 200,"status":"on"},
-         {"url":"https://tenforward.social/@packetcat/tagged/nowplaying.rss","type":"mixed","limit": 450,"status":"on"}
+         {"url":"https://mastodon.social/@Gargron.rss","type":"mixed","limit": 300,"status":"off"},
+         {"url":"https://tenforward.social/@packetcat/tagged/nowplaying.rss","type":"mixed","limit": 300,"status":"on"}
        ],
        [
-         {"url":"https://framasphere.org/public/librazik.atom","type":"mixed","limit": 450,"status":"on"},
+         {"url":"https://framasphere.org/public/librazik.atom","type":"mixed","limit": 240,"status":"on"},
          {"url":"https://botsin.space/@hackaday","type":"photo","limit": 250,"status":"on"}
 
        ],
        [
-         {"url":"https://mastodon.social/@unfa.rss","type":"mixed","limit": 450,"status":"on"},
-         {"url":"https://botsin.space/@tinyskylines.rss","type":"photo","limit": 250,"status":"on"}
+         {"url":"https://mastodon.social/@unfa.rss","type":"mixed","limit": 300,"status":"on"},
+         {"url":"https://botsin.space/@tinyskylines.rss","type":"photo","limit": 280,"status":"on"}
        ]
      ]
     },
     {"name":"music",
      "columns": [
        [
-         {"url":"http://createdigitalmusic.com/feed/","type":"mixed","limit": 450,"status":"off"},
-         {"url":"http://planet.linuxaudio.org/rss20.xml","type":"mixed","limit": 450,"status":"on"},
-         {"url":"https://www.youtube.com/feeds/videos.xml?channel_id=UCAYKj_peyESIMDp5LtHlH2A","type":"mixed","limit": 450,"status":"on"}
+         {"url":"http://planet.linuxaudio.org/rss20.xml","type":"mixed","limit": 300,"status":"on"},
+         {"url":"https://www.youtube.com/feeds/videos.xml?channel_id=UCAYKj_peyESIMDp5LtHlH2A","type":"mixed","limit": 300,"status":"on"}
        ],
        [
          {"url":"https://www.rollingstone.com/music/rss","type":"photo","limit": 200,"status":"on"},
          {"url":"http://www.premierguitar.com/rss/1","type":"mixed","limit": 200,"status":"on"}
        ],
        [
-         {"url":"https://feeds.feedburner.com/dangerousminds/dot/net","type":"photo","limit": 400,"status":"on"}
+         {"url":"https://feeds.feedburner.com/dangerousminds/dot/net","type":"photo","limit": 600,"status":"on"}
        ]
      ]
     }
