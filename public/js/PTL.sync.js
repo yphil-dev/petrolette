4e0d7aa465ca75587e35e02e3c8de214a579b806
yassinphilip <xaccrocheur@gmail.com>
2021-03-17 05:29:21 +0100

    Dropbox & GDrive

--- public/js/PTL.sync.js
+++ public/js/PTL.sync.js
@@ -58,10 +58,10 @@ PTL.sync = (function() {
     modules: [ Feeds ]
   });
 
-  // remoteStorage.setApiKeys({
-  //   dropbox: 'jyss37l88l4ural',
-  //   googledrive: '228755392285-87kkpdod9op50nmnrvnvo6eofr5d3ehl.apps.googleusercontent.com'
-  // });
+  remoteStorage.setApiKeys({
+    dropbox: 'jyss37l88l4ural',
+    googledrive: '228755392285-87kkpdod9op50nmnrvnvo6eofr5d3ehl.apps.googleusercontent.com'
+  });
 
   remoteStorage.on('connected', function() {
     PTL.synchronized = true;
