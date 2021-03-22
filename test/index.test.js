ffcfcfb600435285e95f7144f8ebd447a9f64545
yassinphilip <xaccrocheur@gmail.com>
2021-03-22 10:56:06 +0100

    All tests OK

--- test/index.test.js
+++ test/index.test.js
@@ -5,56 +5,34 @@ const should  = require('should'),
 
 describe('Pétrolette', function() {
 
-  it('Favicon cache created', function(done) {
+  it('Favicon cache dir exists', function(done) {
     fs.access(pjson.FAVICONS_CACHE_DIR, function(err) {
       if (err) return done(err);
       done();
     });
   });
 
-  it('Favicon cache dir writeable', function(done) {
+  it('Favicon cache dir is writeable', function(done) {
     fs.access(pjson.FAVICONS_CACHE_DIR, fs.constants.W_OK, function(err) {
       if (err) return done(err);
       done();
     });
   });
 
-  // it('Pétrolette server is running', function(done) {
-  //   request('http://localhost:8000', { json: true }, (err, res, body) => {
-  //     if (err) return done(err);
-  //     res.statusCode.should.eql(200);
-  //     done();
-  //   });
-  // });
-
   it('Pétrolette server is running', function(done) {
-    fetch('http://localhost:8000').then(function (res) {
-      res.status.should.eql(200);
+    fetch('http://localhost:8000')
+      .then(function (res) {
+        res.status.should.eql(200);
       done();
     }).catch(done);
   });
 
   it('Pétrolette is returning a feed', function(done) {
-    fetch('http://localhost:8000/discover/?url=http://lemonde.fr').then(function (res) {
-      body.should.eql('https://www.lemonde.fr/rss/une.xml');
-      done();
-    }).catch(done);
+    fetch('http://localhost:8000/discover/?url=http://lemonde.fr')
+      .then(async res => {
+        await res.text().should.eventually.eql('https://www.lemonde.fr/rss/une.xml');
+      }).catch(done);
+    done();
   });
-
-  // it('Pétrolette is returning a feed', function(done) {
-  //   request('http://localhost:8000/discover/?url=http://lemonde.fr', { json: true }, (err, res, body) => {
-  //     if (err) return done(err);
-  //     body.should.eql('https://www.lemonde.fr/rss/une.xml');
-  //     done();
-  //   });
-  // });
-
-  // it('Pétrolette is returning a favicon', function(done) {
-  //   request('http://localhost:8000/favicon/?url=http://lemonde.fr', { json: true }, (err, res, body) => {
-  //     if (err) return done(err);
-  //     body.should.eql('https://www.lemonde.fr/favicon.ico');
-  //     done();
-  //   });
-  // });
 
 });
