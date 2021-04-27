007ba7ce9dcfa167e40e2924b5e0498080864764
yassinphilip <xaccrocheur@gmail.com>
2021-04-27 09:58:52 +0000

    Huge theme update/cleanup - ui-state-*

--- public/js/PTL.main.js
+++ public/js/PTL.main.js
@@ -403,7 +403,7 @@ var PTL = (function() {
               PTL.dialog.beg();
             });
 
-      // $debugHiddenButton.appendTo('body');
+      $debugHiddenButton.appendTo('body');
 
     },
     sideMenu: function(action) {
