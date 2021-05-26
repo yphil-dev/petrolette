e624e10591cf59b7c0c3d66432a39aa7f5d3dfe5
yassinphilip <xaccrocheur@gmail.com>
2021-05-26 19:30:46 +0100

    Tests

--- petrolette.js
+++ petrolette.js
@@ -9,15 +9,15 @@ const express = require('express'),
       compression = require('compression');
 
 function ensureExists(path, mask, cb) {
-    if (typeof mask == 'function') { // Allow the `mask` parameter to be optional
+    if (typeof mask == 'function') { 
         cb = mask;
         mask = 0777;
     }
     fs.mkdir(path, mask, function(err) {
         if (err) {
-            if (err.code == 'EEXIST') cb(null); // Ignore the error if the folder already exists
-            else cb(err); // Something else went wrong
-        } else cb(null); // Successfully created folder
+            if (err.code == 'EEXIST') cb(null); 
+            else cb(err); 
+        } else cb(null); 
     });
 }
 
