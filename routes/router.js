7174fb063d84e6841d7f746d5d8293b09c4c3961
yassinphilip <xaccrocheur@gmail.com>
2021-09-08 02:57:40 +0100

    v1.4.9
    - Deps update
    - Feedrat refactoring
      - Youtube specifics are now useless / obsolete
      - Search feeds is now Pétrolette's job and no longer feedrat's
    - #Bug: Lang menu preselection

--- routes/router.js
+++ routes/router.js
@@ -3,9 +3,9 @@ const express = require('express'),
       feeder = require('./feeder'),
       fetch = require('node-fetch'),
       favrat = require('favrat'),
-      // favrat = require(__dirname + '/../../favrat/'),
+      // favrat = require('/home/px/src/favrat/'),
       feedrat = require('feedrat'),
-      // feedrat = require(__dirname + '/../../feedrat/'),
+      // feedrat = require('/home/px/src/feedrat/'),
       fs = require('fs'),
       path = require('path'),
       crypto = require('crypto'),
@@ -23,101 +23,107 @@ router.use(sanitize);
 
 router.get('/favicon', function(req, res) {
 
-  favrat(req.query.url, async function(error, url) {
+    favrat(req.query.url, async function(error, url) {
 
-    if (error) {
-      res.status(500).send(error);
-    } else if (url) {
+        if (error) {
+            res.status(500).send(error);
+        } else if (url) {
 
-      const hash = crypto.createHash('md5').update(url).digest('hex'),
-            fileName = hash + '.favicon',
-            filePath = path.join(pjson.FAVICONS_CACHE_DIR, fileName);
+            const hash = crypto.createHash('md5').update(url).digest('hex'),
+                  fileName = hash + '.favicon',
+                  filePath = path.join(pjson.FAVICONS_CACHE_DIR, fileName);
 
-      try {
+            try {
 
-        const response = await fetch(url);
-        const fileStream = fs.createWriteStream(filePath);
+                const response = await fetch(url);
+                const fileStream = fs.createWriteStream(filePath);
 
-        response.body.pipe(fileStream);
-        response.body.on("error", () => {
-          res.status(500).send(error);
-        });
-        fileStream.on("finish", () => {
-          res.send(hash);
-        });
+                response.body.pipe(fileStream);
+                response.body.on("error", () => {
+                    res.status(500).send(error);
+                });
+                fileStream.on("finish", () => {
+                    res.send(hash);
+                });
 
-      } catch (err) {
-        res.status(500).send(error);
-      }
-      
-    }
-    
-  });
+            } catch (err) {
+                res.status(500).send(error);
+            }
+            
+        }
+        
+    });
 });
 
 router.use(morgan('combined'));
 
 router.get('/feed', function(req, res) {
 
-  feeder.getFeed(req.query.url, req.query.lastItem, function (error, feedItems, feedTitle, feedLink, lastItem, totalNewItems) {
+    feeder.getFeed(req.query.url, req.query.lastItem, function (error, feedItems, feedTitle, feedLink, lastItem, totalNewItems) {
 
-    if (feedItems && !res.headersSent) {
-      res.send({
-        feedItems: feedItems,
-        feedLink: feedLink,
-        feedTitle: feedTitle,
-        lastItem: lastItem,
-        totalNewItems: totalNewItems
-      });
+        if (feedItems && !res.headersSent) {
+            res.send({
+                feedItems: feedItems,
+                feedLink: feedLink,
+                feedTitle: feedTitle,
+                lastItem: lastItem,
+                totalNewItems: totalNewItems
+            });
 
-    } else if (error && !res.headersSent) {
-      res.send({ error: error });
-    }
-    
-  });
+        } else if (error && !res.headersSent) {
+            res.send({ error: error });
+        }
+        
+    });
 });
 
 router.get('/robots.txt', function (req, res) {
-  res.type('text/plain');
-  res.send("User-agent: *\nDisallow: /feed\nDisallow: /discover\nDisallow: /favicon");
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow: /feed\nDisallow: /discover\nDisallow: /favicon");
 });
 
 router.get('/discover', function(req, res) {
 
-  feedrat(req.query.url, req.query.searchPrefix, function(err, feed) {
-
-    if (feed) {
-      res.send(feed);
-    } else if (err) {
-      res.status(500).send(err.code);
-    } else {
-      res.status(500).send('No feed found');
+    try {
+        var p = new URL(req.query.url);
+    } catch (error) {
+        return res.send(new String(req.query.searchPrefix + req.query.url.split(' ')));
     }
 
-  });
+    feedrat(req.query.url, function(err, url) {
+        
+        if (err) {
+            res.status(500).send(err.code);
+        } else if (url) {
+            res.send(url);
+        } else {
+            res.status(500).send('No feed found');
+        }
+
+    });
 });
 
 router.get('/about/javascript', function(req, res) {
-  res.render('javascript');
+    res.render('javascript');
 });
 
 router.get('/', function(req, res) {
-  res.render('index', {
-    queryString: req.query.add,
-    version: pjson.version,
-    favratversion: favratpjson.version,
-    feedratversion: feedratpjson.version
-  });
+    res.render('index', {
+        queryString: req.query.add,
+        version: pjson.version,
+        favratversion: favratpjson.version,
+        feedratversion: feedratpjson.version
+    });
 });
 
 router.use(function(req, res) {
-  console.error('404 req: %s (%s)', req.url);
-  res.status(404).send('404: Page not Found');
+    console.error('404 req: %s (%s)', req.url);
+    res.status(404).send('404: Page not Found');
 });
 
 router.use(function(error, req, res, next) {
-  console.error('500 req: %s (%s)', req.url);
-  res.status(500).send('500: whoa! Internal Server Error');
+    console.error('500 req: %s (%s)', req.url);
+    res.status(500).send('500: whoa! Internal Server Error');
 });
 
 module.exports = router;
