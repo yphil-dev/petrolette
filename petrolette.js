46a34dc4a3302a9c39b00dac5e1a6d34463a79bc
yassinphilip <xaccrocheur@gmail.com>
2022-02-25 11:41:16 +0100

    Temp image CORS problem fix: crossOriginEmbedderPolicy: false

--- petrolette.js
+++ petrolette.js
@@ -24,7 +24,7 @@ app.set('view engine', 'html');
 
 app.use(compression());
 
-app.use(helmet({contentSecurityPolicy: false, crossOriginOpenerPolicy: false}));
+app.use(helmet({contentSecurityPolicy: false, crossOriginEmbedderPolicy: false,}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
