9f964fe170f59b5e65b865e30902af0a93c211af
yassinphilip <xaccrocheur@gmail.com>
2022-02-25 10:42:20 +0100

    Temp image CORS problem fix

--- public/js/PTL.feed.js
+++ public/js/PTL.feed.js
@@ -315,7 +315,7 @@ PTL.feed = {
 
                 if (item.enclosures && typeof item.enclosures[0] !== 'undefined' && item.enclosures[0].url) {
 
-                    if (item.enclosures[0].url && item.enclosures[0].url.match(/(\.jpg|\.png|\.gif|\.jpeg)/)) {
+                    if (item.enclosures[0].url && item.enclosures[0].url.endsWith(".jpg")) {
                         imageUrl = item.enclosures[0].url;
                     }
 
