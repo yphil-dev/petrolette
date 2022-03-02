a830db845e891e6148adf9d5c04ee77740d6dbf5
yassinphilip <xaccrocheur@gmail.com>
2022-03-02 02:35:38 +0100

    CSP rules WIP (audio)

--- petrolette.js
+++ petrolette.js
@@ -29,7 +29,7 @@ app.use(compression());
 app.use(
     helmet.contentSecurityPolicy({
         directives: {
-            defaultSrc: ["'self'", "https://www.googleapis.com"],
+            defaultSrc: ["'self'", "https://www.googleapis.com", "https://api.dropboxapi.com", "https://content.dropboxapi.com", "https:"],
             imgSrc: ["'self'", "'unsafe-inline'", "https:", "data:"],
             styleSrc: ["'self'", "'unsafe-inline'"],
             scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
