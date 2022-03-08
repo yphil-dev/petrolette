aa921ed211ab6685ee1bfb84bd0e93144f72fd13
yassinphilip <xaccrocheur@gmail.com>
2022-03-08 13:42:55 +0100

    Testing Tests

--- petrolette.js
+++ petrolette.js
@@ -23,6 +23,8 @@ app.set('view engine', 'html');
 
 app.use(compression());
 
+app.use(helmet({contentSecurityPolicy: false, crossOriginEmbedderPolicy: false}));
+
 // app.use(helmet({contentSecurityPolicy: false, crossOriginEmbedderPolicy: false}));
 
 // app.use(
