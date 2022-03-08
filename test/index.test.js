65de790018e7d5707de78db43390da81c3628a0c
yassinphilip <xaccrocheur@gmail.com>
2022-03-08 13:30:13 +0100

    Testing Tests

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ const should = require('should'),
     pjson = require('../package.json'),
     fetch = require('node-fetch');
 
-const PORT = pjson.HTTPS_PORT;
+const PORT = pjson.HTTP_PORT;
 
 describe('Pétrolette', function() {
 
@@ -22,7 +22,7 @@ describe('Pétrolette', function() {
     // });
 
     it('Pétrolette server is running', function(done) {
-        fetch('https://localhost:' + PORT)
+        fetch('http://localhost:' + PORT)
             .then(function(res) {
                 res.status.should.eql(200);
                 done();
@@ -30,7 +30,7 @@ describe('Pétrolette', function() {
     });
 
     it('test', function(done) {
-        fetch('https://localhost:' + PORT)
+        fetch('http://localhost:' + PORT)
             .then(res => {
                 res.status.should.eql(200);
             }).catch(done);
