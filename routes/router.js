e92b7536f01e1d6441e6819b9f3b8576420d5c41
yassinphilip <xaccrocheur@gmail.com>
2022-03-09 23:00:30 +0100

    Updated tests: Actual feed

--- routes/router.js
+++ routes/router.js
@@ -1,18 +1,18 @@
 const express = require('express'),
-  router = express.Router(),
-  feeder = require('./feeder'),
-  fetch = require('node-fetch'),
-  favrat = require('favrat'),
-  feedrat = require('feedrat'),
-  // feedrat = require('/home/px/src/feedrat/'),
-  fs = require('fs'),
-  path = require('path'),
-  crypto = require('crypto'),
-  pjson = require('../package.json'),
-  favratpjson = require('../node_modules/favrat/package.json'),
-  feedratpjson = require('../node_modules/feedrat/package.json'),
-  sanitize = require('sanitize').middleware,
-  morgan = require('morgan');
+    router = express.Router(),
+    feeder = require('./feeder'),
+    fetch = require('node-fetch'),
+    favrat = require('favrat'),
+    feedrat = require('feedrat'),
+    // feedrat = require('/home/px/src/feedrat/'),
+    fs = require('fs'),
+    path = require('path'),
+    crypto = require('crypto'),
+    pjson = require('../package.json'),
+    favratpjson = require('../node_modules/favrat/package.json'),
+    feedratpjson = require('../node_modules/feedrat/package.json'),
+    sanitize = require('sanitize').middleware,
+    morgan = require('morgan');
 
 console.error('### (re)START ## Version (%s)', pjson.version);
 
@@ -22,110 +22,110 @@ router.use(sanitize);
 
 router.get('/favicon', function(req, res) {
 
-  favrat(req.query.url, async function(error, url) {
+    favrat(req.query.url, async function(error, url) {
 
-    if (error) {
-      res.status(500).send(error);
-    } else if (url) {
+        if (error) {
+            res.status(500).send(error);
+        } else if (url) {
 
-      const hash = crypto.createHash('md5').update(url).digest('hex'),
-        fileName = hash + '.favicon',
-        filePath = path.join(pjson.FAVICONS_CACHE_DIR, fileName);
+            const hash = crypto.createHash('md5').update(url).digest('hex'),
+                fileName = hash + '.favicon',
+                filePath = path.join(pjson.FAVICONS_CACHE_DIR, fileName);
 
-      try {
+            try {
 
-        const response = await fetch(url);
-        const fileStream = fs.createWriteStream(filePath);
+                const response = await fetch(url);
+                const fileStream = fs.createWriteStream(filePath);
 
-        response.body.pipe(fileStream);
-        response.body.on("error", () => {
-          res.status(500).send(error);
-        });
-        fileStream.on("finish", () => {
-          res.send(hash);
-        });
+                response.body.pipe(fileStream);
+                response.body.on("error", () => {
+                    res.status(500).send(error);
+                });
+                fileStream.on("finish", () => {
+                    res.send(hash);
+                });
 
-      } catch (err) {
-        res.status(500).send(error);
-      }
+            } catch (err) {
+                res.status(500).send(error);
+            }
 
-    }
+        }
 
-  });
+    });
 });
 
 router.use(morgan('combined'));
 
 router.get('/feed', function(req, res) {
 
-  feeder.getFeed(req.query.url, req.query.lastItem, function(error, feedItems, feedTitle, feedLink, lastItem, totalNewItems) {
+    feeder.getFeed(req.query.url, req.query.lastItem, function(error, feedItems, feedTitle, feedLink, lastItem, totalNewItems) {
 
-    if (feedItems && !res.headersSent) {
-      res.send({
-        feedItems: feedItems,
-        feedLink: feedLink,
-        feedTitle: feedTitle,
-        lastItem: lastItem,
-        totalNewItems: totalNewItems
-      });
+        if (feedItems && !res.headersSent) {
+            res.send({
+                feedItems: feedItems,
+                feedLink: feedLink,
+                feedTitle: feedTitle,
+                lastItem: lastItem,
+                totalNewItems: totalNewItems
+            });
 
-    } else if (error && !res.headersSent) {
-      res.send({ error: error });
-    }
+        } else if (error && !res.headersSent) {
+            res.send({ error: error });
+        }
 
-  });
+    });
 });
 
 router.get('/robots.txt', function(req, res) {
-  res.type('text/plain');
-  res.send("User-agent: *\nDisallow: /feed\nDisallow: /discover\nDisallow: /favicon");
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow: /feed\nDisallow: /discover\nDisallow: /favicon");
 });
 
 router.get('/discover', function(req, res) {
 
-  try {
-    new URL(req.query.url);
-  } catch (error) {
-    let feeds = [];
-    feeds.push(req.query.searchPrefix + req.query.url.split(' '));
-    return res.send(feeds);
-  }
-
-  feedrat(req.query.url, function(err, url) {
-
-    if (err) {
-      console.error('err.message: %s (%s)', err.message);
-      res.status(500).send(err.code);
-    } else if (url) {
-      res.send(url);
-    } else {
-      res.send({ error: error });
+    try {
+        new URL(req.query.url);
+    } catch (error) {
+        let feeds = [];
+        feeds.push(req.query.searchPrefix + req.query.url.split(' '));
+        return res.send(feeds);
     }
 
-  });
+    feedrat(req.query.url, function(err, url) {
+
+        if (err) {
+            console.error('err.message: %s (%s)', err.message);
+            res.status(500).send(err.code);
+        } else if (url) {
+            res.send(url);
+        } else {
+            res.send({ error: error });
+        }
+
+    });
 });
 
 router.get('/about/javascript', function(req, res) {
-  res.render('javascript');
+    res.render('javascript');
 });
 
 router.get('/', function(req, res) {
-  res.render('index', {
-    queryString: req.query.add,
-    version: pjson.version,
-    favratversion: favratpjson.version,
-    feedratversion: feedratpjson.version
-  });
+    res.render('index', {
+        queryString: req.query.add,
+        version: pjson.version,
+        favratversion: favratpjson.version,
+        feedratversion: feedratpjson.version
+    });
 });
 
 router.use(function(req, res) {
-  console.error('404 req: %s (%s)', req.url);
-  res.status(404).send('404: Page not Found');
+    console.error('404 req: %s (%s)', req.url);
+    res.status(404).send('404: Page not Found');
 });
 
 router.use(function(error, req, res, next) {
-  console.error('500 req: %s (%s)', req.url);
-  res.status(500).send('500: whoa! Internal Server Error');
+    console.error('500 req: %s (%s)', req.url);
+    res.status(500).send('500: whoa! Internal Server Error');
 });
 
 module.exports = router;
--- test/index.test.js
+++ test/index.test.js
@@ -7,29 +7,21 @@ const PORT = pjson.HTTP_PORT;
 
 describe('Pétrolette', function() {
 
-    // it('Favicon cache dir exists', function(done) {
-    //     fs.access(pjson.FAVICONS_CACHE_DIR, function(err) {
-    //         if (err) return done(err);
-    //         done();
-    //     });
-    // });
+    it('Favicon cache dir exists', function(done) {
+        fs.access(pjson.FAVICONS_CACHE_DIR, function(err) {
+            if (err) return done(err);
+            done();
+        });
+    });
 
-    // it('Favicon cache dir is writeable', function(done) {
-    //     fs.access(pjson.FAVICONS_CACHE_DIR, fs.constants.W_OK, function(err) {
-    //         if (err) return done(err);
-    //         done();
-    //     });
-    // });
+    it('Favicon cache dir is writeable', function(done) {
+        fs.access(pjson.FAVICONS_CACHE_DIR, fs.constants.W_OK, function(err) {
+            if (err) return done(err);
+            done();
+        });
+    });
 
-    // it('Pétrolette server is running', function(done) {
-    //     fetch('http://localhost:' + PORT)
-    //         .then(function(res) {
-    //             res.status.should.eql(200);
-    //             done();
-    //         }).catch(done);
-    // });
-
-    it('Docker test', function(done) {
+    it('Pétrolette server is running', function(done) {
         fetch('http://localhost:' + PORT)
             .then(res => {
                 res.status.should.eql(200);
@@ -37,7 +29,24 @@ describe('Pétrolette', function() {
         done();
     });
 
-    it('Pétrolette is returning a feed', function(done) {
+    it('Pétrolette server is running', function(done) {
+        fetch('http://localhost:' + PORT + '/favicon/?url=https://yphil.bitbucket.io/feed.xml')
+            .then(res => {
+                res.status.should.eql(200);
+            }).catch(done);
+        done();
+    });
+
+    it('Pétrolette is returning a favicon URL hash', function(done) {
+        fetch('http://localhost:' + PORT + '/favicon/?url=https://yphil.bitbucket.io/feed.xml')
+            .then(res => res.text())
+            .then(text => {
+                text.should.eql('a210177714b9fd9e35bfd7241eb32214');
+            }).catch(done);
+        done();
+    });
+
+    it('Pétrolette is returning a feed URL', function(done) {
         fetch('http://localhost:' + PORT + '/discover/?url=https://yphil.bitbucket.io/')
             .then(res => res.json())
             .then(res => {
@@ -46,13 +55,22 @@ describe('Pétrolette', function() {
         done();
     });
 
-    // it('Pétrolette is returning several feeds', function(done) {
-    //     fetch('http://localhost:' + PORT + '/discover/?url=https://yphil.bitbucket.io/')
-    //         .then(res => res.json())
-    //         .then(res => {
-    //             res.length.should.eql(2);
-    //         }).catch(done);
-    //     done();
-    // });
+    it('Pétrolette is returning several feed URLs', function(done) {
+        fetch('http://localhost:' + PORT + '/discover/?url=https://yphil.bitbucket.io/')
+            .then(res => res.json())
+            .then(res => {
+                res.length.should.eql(2);
+            }).catch(done);
+        done();
+    });
+
+    it('Pétrolette is returning an actual feed', function(done) {
+        fetch('http://localhost:' + PORT + '/feed/?url=https://yphil.bitbucket.io/feed.xml')
+            .then(res => res.text())
+            .then(text => {
+                text.substring(2, 11).should.eql('feedItems');
+            }).catch(done);
+        done();
+    });
 
 });
