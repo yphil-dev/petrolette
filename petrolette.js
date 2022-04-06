c7f92cf88a1191f0ab25d3054b35ce72dae6f5f6
yassinphilip <xaccrocheur@gmail.com>
2022-04-06 13:28:58 +0000

    CSP: Back to the drawing board

--- petrolette.js
+++ petrolette.js
@@ -26,47 +26,47 @@ app.use(compression());
 
 // app.use(helmet({contentSecurityPolicy: false, crossOriginEmbedderPolicy: false}));
 
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'", "https://www.googleapis.com", "https://api.dropboxapi.com", "https://content.dropboxapi.com", "https:"],
-      imgSrc: ["'self'", "'unsafe-inline'", "https:", "data:"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
-      scriptSrc: ["'self'", "'unsafe-inline'"],
-      scriptSrcAttr: null
-    }
-  })
-);
-
 // app.use(
-//   helmet({
-//     crossOriginEmbedderPolicy: false,
-//     // crossOriginResourcePolicy: false,
-//     contentSecurityPolicy: {
-//       useDefaults: true,
-//       directives: {
-//         defaultSrc: [
-//           "'self'",
-//           "https:",
-//           "'unsafe-inline'",
-//           "https://www.googleapis.com",
-//           "https://api.dropboxapi.com",
-//           "https://content.dropboxapi.com"
-//         ],
-//         scriptSrc: null,
-//         imgSrc: [
-//           "'self'",
-//           "https:",
-//           "'unsafe-inline'",
-//           "data:"
-//         ],
-//         styleSrc: null,
-//         scriptSrcAttr: null
-//       },
-//       reportOnly: false,
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'", "https://www.googleapis.com", "https://api.dropboxapi.com", "https://content.dropboxapi.com", "https:"],
+//       imgSrc: ["'self'", "'unsafe-inline'", "https:", "data:"],
+//       styleSrc: ["'self'", "'unsafe-inline'"],
+//       scriptSrc: ["'self'", "'unsafe-inline'"],
+//       scriptSrcAttr: null
 //     }
 //   })
 // );
+
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+    // crossOriginResourcePolicy: false,
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        defaultSrc: [
+          "'self'",
+          "https:",
+          "'unsafe-inline'",
+          "https://www.googleapis.com",
+          "https://api.dropboxapi.com",
+          "https://content.dropboxapi.com"
+        ],
+        scriptSrc: null,
+        imgSrc: [
+          "'self'",
+          "https:",
+          "'unsafe-inline'",
+          "data:"
+        ],
+        styleSrc: null,
+        scriptSrcAttr: null
+      },
+      reportOnly: false,
+    }
+  })
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
