4e814950ba257a7d88ff3672b4fbbcade7bf5789
yassinphilip <xaccrocheur@gmail.com>
2022-09-08 20:22:37 +0100

    CI test

--- http/local.js
+++ http/local.js
@@ -5,16 +5,15 @@ const petrolette = require('../petrolette'),
 
 const httpServer = http.createServer(petrolette);
 
-const myArgs = process.argv.slice(2);
-console.error('myArgs: ', myArgs[0]);
-
 const portHttp =  pjson.HTTP_PORT || 8000;
 const portHttps =  pjson.HTTPS_PORT || 8001;
+
+console.error('HTTP!');
 
 process.on('uncaughtException', function(err) {
   console.error('### Pétrolette uncaughtException: %s', err.code);
 });
 
-httpServer.listen(portHttp, (req, res) => {
+httpServer.listen(portHttp, () => {
   console.debug('HTTP Server running on port %s', portHttp);
 });
