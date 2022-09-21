7d27fc43ec39dcd09c2d0673e288d6ad57e9c452
yassinphilip <xaccrocheur@gmail.com>
2022-09-21 16:30:59 +0100

    Dialog: Beg: s/OK/Later

--- public/js/PTL.i18n.js
+++ public/js/PTL.i18n.js
@@ -22,6 +22,9 @@ PTL.i18n.translations = {
   "Refresh": { fr: "Rafraîchir", ja: "更新", es: "Actualizar" },
   "Reset": { fr: "Réinitialiser", ja: "リセット", es: "Reiniciar" },
 
+  "Later": { fr: "Plus tard", ja: "後で", es: "Luego" },
+
+  
   "Name": { fr: "Nom", ja: "姓", es: "Nombre" },
   "Names": { fr: "Noms", ja: "お名前 (必須)", es: "Nombres" },
   "Position": { fr: "Position", ja: "ポジション", es: "Posición" },
