499fcbd1522d9db3b972eed479b9a32df1eeb453
yassinphilip <xaccrocheur@gmail.com>
2023-05-18 12:48:49 +0100

    Version bump everything but fetch and rs

--- public/js/PTL.dialog.js
+++ public/js/PTL.dialog.js
@@ -514,7 +514,7 @@ PTL.dialog = {
           {
             text: PTL.tr('Suggestions'),
             title: PTL.tr('Suggestions'),
-            class: 'translate ui-state-success',
+            class: 'translate ui-state-highlight',
             click: function() {
               clearWindow(function() {
                 $('i.clearWindowSpinner').remove();
