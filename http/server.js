1ae6c5cc0d4a22f3f192035ee2301bff68a10b5d
yassinphilip <xaccrocheur@gmail.com>
2024-12-09 23:12:02 +0100

    Render: Migrating to a full express app

--- http/server.js
+++ http/server.js
@@ -1,49 +1,15 @@
-const petrolette = require('../petrolette'),
-      pjson = require('../package.json'),
-      http = require('http'),
-      https = require('https'),
-      fs = require('fs');
+const petrolette = require('../petrolette');
+const http = require('http');
 
-const portHttp =  pjson.HTTP_PORT || 8000;
-const portHttps =  pjson.HTTPS_PORT || 8001;
+const port = process.env.PORT || 8000;
 
-process.on('uncaughtException', function(err) {
-  console.error('## Pétrolette uncaughtException: %s', JSON.stringify(err));
+process.on('uncaughtException', function (err) {
+  console.error('## Pétrolette uncaughtException:', err);
 });
 
-console.error('## Pétrolette environment: %s', process.env.NODE_ENV);
+console.error('## Pétrolette environment:', process.env.NODE_ENV);
 
-process.on('uncaughtException', function(err) {
-  console.error('## Pétrolette uncaughtException: %s', err.code);
+const server = http.createServer(petrolette);
+server.listen(port, () => {
+  console.error('## Pétrolette Server running on port', port);
 });
-
-if (process.env.NODE_ENV == 'development') {
-  const httpServer = http.createServer(petrolette);
-  httpServer.listen(portHttp, () => {
-    console.error('## Pétrolette HTTP Server running on port %s', portHttp);
-  });
-	
-} else {
-
-  try {
-    const httpsServer = https.createServer({
-      key: fs.readFileSync('/etc/letsencrypt/live/petrolette.space/privkey.pem'),
-      cert: fs.readFileSync('/etc/letsencrypt/live/petrolette.space/cert.pem'),
-      ca: fs.readFileSync('/etc/letsencrypt/live/petrolette.space/chain.pem'),
-    }, petrolette);
-
-    httpsServer.listen(portHttps, () => {
-      console.error('## Pétrolette HTTPS Server running on port %s', portHttps);
-    });
-
-  } catch (error) {
-    console.error('HTTPS error: %s', error);
-  }
-
-  http.createServer(function (req, res) {
-			console.error('## Pétrolette HTTP server running on %s and NOT redirecting to %s', portHttp, portHttps);
-			res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-  }).listen(portHttp);
-
-}
